046ff8c94a2f0d37dc20b1a846d617054f0403ec
Victor Huynh <45438361+victor-h-huynh@users.noreply.github.com>
2020-11-25 22:15:46 -0500

    inital commit

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,16 @@
+    let js = 'amazing';
+    if(js === 'amazing')
+
+    console.log(40 + 8 + 23 - 10);
+
+    console.log("Jonas");
+    console.log(23)
+
+    let firstName = "Matilda";
+    
+
+    console.log(firstName);
+    console.log(firstName);
+    console.log(firstName);
+
+    
