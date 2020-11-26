94af5fd715da4e986c6fe155e7771f4ee27f11c8
Victor Huynh <45438361+victor-h-huynh@users.noreply.github.com>
2020-11-25 22:45:06 -0500

    fundamentals-part-1

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -13,4 +13,11 @@
     console.log(firstName);
     console.log(firstName);
 
-    
+    let jonas_matilda = "JM";
+    let $function = 27;
+
+    let person = "jonas";
+    let PI = 3.1415;
+
+    let myFirstJob = "Programmer";
+    let myCurrentJob = "Teacher";
