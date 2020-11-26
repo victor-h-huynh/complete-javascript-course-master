f2f38c3f1e8951a66edab484a6ef02455ae2675f
Victor Huynh <45438361+victor-h-huynh@users.noreply.github.com>
2020-11-26 14:31:26 -0500

    datatypes

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -29,3 +29,23 @@
 // console.log(country);
 // console.log(continent);
 // console.log(population);
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun)
+
+// console.log(typeof true);
+console.log(typeof javascriptIsFun);
+// console.log(typeof 23);
+// console.log(typeof 'Jonas');
+
+javascriptIsFun = 'YES!';
+console.log(typeof javascriptIsFun)
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(typeof year);
+
+console.log(typeof null);
