7494e4502817b90d2fa7e7c862e246acae5c12f7
Victor Huynh <45438361+victor-h-huynh@users.noreply.github.com>
2020-11-25 23:24:29 -0500

    values and variables

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,23 +1,31 @@
-    let js = 'amazing';
-    if(js === 'amazing')
+    // let js = 'amazing';
+    // if(js === 'amazing')
 
-    console.log(40 + 8 + 23 - 10);
+    // console.log(40 + 8 + 23 - 10);
 
-    console.log("Jonas");
-    console.log(23)
+    // console.log("Jonas");
+    // console.log(23)
 
-    let firstName = "Matilda";
+    // let firstName = "Matilda";
     
 
-    console.log(firstName);
-    console.log(firstName);
-    console.log(firstName);
+    // console.log(firstName);
+    // console.log(firstName);
+    // console.log(firstName);
 
-    let jonas_matilda = "JM";
-    let $function = 27;
+    // let jonas_matilda = "JM";
+    // let $function = 27;
 
-    let person = "jonas";
-    let PI = 3.1415;
+    // let person = "jonas";
+    // let PI = 3.1415;
 
-    let myFirstJob = "Programmer";
-    let myCurrentJob = "Teacher";
+    // let myFirstJob = "Programmer";
+    // let myCurrentJob = "Teacher";
+
+let country = "Canada";
+let continent = "North America";
+let population = 579;
+
+console.log(country);
+console.log(continent);
+console.log(population);
