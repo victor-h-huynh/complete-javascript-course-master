8b8252e30aeed8a2749210e78ea6bdf8d6ac3c60
Victor Huynh <45438361+victor-h-huynh@users.noreply.github.com>
2020-11-29 20:48:09 -0500

    js-fundamentals

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -30,22 +30,25 @@
 // console.log(continent);
 // console.log(population);
 
-let javascriptIsFun = true;
-console.log(javascriptIsFun)
+// let javascriptIsFun = true;
+// console.log(javascriptIsFun)
 
 // console.log(typeof true);
-console.log(typeof javascriptIsFun);
+// console.log(typeof javascriptIsFun);
 // console.log(typeof 23);
 // console.log(typeof 'Jonas');
 
-javascriptIsFun = 'YES!';
-console.log(typeof javascriptIsFun)
+// javascriptIsFun = 'YES!';
+// console.log(typeof javascriptIsFun)
 
-let year;
-console.log(year);
-console.log(typeof year);
+// let year;
+// console.log(year);
+// console.log(typeof year);
 
-year = 1991;
-console.log(typeof year);
+// year = 1991;
+// console.log(typeof year);
 
-console.log(typeof null);
+// console.log(typeof null);
+
+let age = 30;
+age = 31;
