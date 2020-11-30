c994498d20074090df24cfa34190fe362d6c535e
Victor Huynh <45438361+victor-h-huynh@users.noreply.github.com>
2020-11-29 20:55:58 -0500

    let-const-var

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -52,3 +52,13 @@
 
 let age = 30;
 age = 31;
+
+const birthYear = 1991;
+// birthYear = 1990
+// const job;
+
+var job = 'programmer';
+job = 'teacher';
+
+lastName = 'Doe';
+console.log(lastName);
