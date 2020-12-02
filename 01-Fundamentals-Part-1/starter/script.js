987cfcdab3251da6489f80d7bceb44222d05101a
Victor Huynh <45438361+victor-h-huynh@users.noreply.github.com>
2020-12-02 10:55:28 -0500

    strings-and-template-literals

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -147,3 +147,26 @@
 // console.log(markBMI);
 // console.log(johnBMI)
 // console.log(markHigherBMI)
+
+const firstName = 'Jonas';
+const job = 'teacher';
+const birthYear = 1991;
+const year = 2037
+
+const jonas = "I'm " + firstName + ', a ' + (year - birthYear) + ' year old ' + job + '!';
+
+console.log(jonas);
+
+const jonasNew = `I'm ${firstName}, a ${(year - birthYear)} year old ${job}!`
+
+console.log(jonasNew);
+
+console.log(`Just a regular string...`);
+
+console.log('String with \n\
+multiple \n\
+lines');
+
+console.log(`String
+multiple
+lines`)
