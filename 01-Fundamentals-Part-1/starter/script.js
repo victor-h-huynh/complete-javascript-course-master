09894095c04f4c7c56030ef11eea5a1c13d1032e
Victor Huynh <45438361+victor-h-huynh@users.noreply.github.com>
2020-12-05 14:46:27 -0500

    type-coversation-coercion

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -247,3 +247,8 @@ console.log(typeof NaN);
 console.log(String(23), 23);
 
 // type coercion
+console.log('I am ' + 23 + ' years old');
+console.log('23' - '10' - 3);
+console.log('23' * '2');
+console.log('23' / '2');
+console.log('23' > '18');
