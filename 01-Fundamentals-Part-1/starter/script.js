ee80404c43749a42cc9b0f0482e53514fdbbaae4
Victor Huynh <45438361+victor-h-huynh@users.noreply.github.com>
2020-12-05 16:06:02 -0500

    equality-operator

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -259,22 +259,26 @@
 
 // 5 falsy values: 0, '', undefined, null NaN
 
-console.log(Boolean(0));
-console.log(Boolean(undefined));
-console.log(Boolean('Jonas'));
-console.log(Boolean({}));
-console.log(Boolean(''));
+// console.log(Boolean(0));
+// console.log(Boolean(undefined));
+// console.log(Boolean('Jonas'));
+// console.log(Boolean({}));
+// console.log(Boolean(''));
 
-const money = 0;
-if (money) {
-    console.log("Don't spend it all ;)");
-} else {
-    console.log("You should get a job");
-}
+// const money = 0;
+// if (money) {
+//     console.log("Don't spend it all ;)");
+// } else {
+//     console.log("You should get a job");
+// }
 
-let height;
-if (height) {
-    console.log('YAY! Height is defined');
-} else {
-    console.log('Height is UNDEFINED');
-}
+// let height;
+// if (height) {
+//     console.log('YAY! Height is defined');
+// } else {
+//     console.log('Height is UNDEFINED');
+// }
+
+///////////////////////////////////////////////////////////////////////////////////////
+// Equality Operators: == vs ===
+///////////////////////////////////////////////////////////////////////////////////////
