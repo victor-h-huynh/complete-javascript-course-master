75a9e716d671bc8f569017c8543695b514552316
Victor Huynh <45438361+victor-h-huynh@users.noreply.github.com>
2020-12-05 14:16:39 -0500

    coding-challenge-2

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -179,21 +179,21 @@
 // Taking decisions: if / else Statements
 ///////////////////////////////////////////////////////////////////////////////////////
 
-const age = 19; 
+// const age = 19; 
 
-if(age >= 18) {
-    console.log('Sarah can start driving license');
-} else {
-    const yearsLeft = 18 - age;
-    console.log(`Sarah is too young. Wait another ${yearsLeft} years :)`)
-}
+// if(age >= 18) {
+//     console.log('Sarah can start driving license');
+// } else {
+//     const yearsLeft = 18 - age;
+//     console.log(`Sarah is too young. Wait another ${yearsLeft} years :)`)
+// }
 
-const birthYear = 1998;
-let century;
-if(birthYear <= 2000) {
-    century = 20;
-} else {
-    century = 21;
-}
+// const birthYear = 1998;
+// let century;
+// if(birthYear <= 2000) {
+//     century = 20;
+// } else {
+//     century = 21;
+// }
 
-console.log(century)
+// console.log(century)
