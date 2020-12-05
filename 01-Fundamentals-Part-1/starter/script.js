59cd736de74a15f84f4168213c419f4442304b57
Victor Huynh <45438361+victor-h-huynh@users.noreply.github.com>
2020-12-05 16:03:48 -0500

    truthy-falsy-values

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -256,3 +256,25 @@
 ///////////////////////////////////////////////////////////////////////////////////////
 // Truthy and Falsy Values
 ///////////////////////////////////////////////////////////////////////////////////////
+
+// 5 falsy values: 0, '', undefined, null NaN
+
+console.log(Boolean(0));
+console.log(Boolean(undefined));
+console.log(Boolean('Jonas'));
+console.log(Boolean({}));
+console.log(Boolean(''));
+
+const money = 0;
+if (money) {
+    console.log("Don't spend it all ;)");
+} else {
+    console.log("You should get a job");
+}
+
+let height;
+if (height) {
+    console.log('YAY! Height is defined');
+} else {
+    console.log('Height is UNDEFINED');
+}
