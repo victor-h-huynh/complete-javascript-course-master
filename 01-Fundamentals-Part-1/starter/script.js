167d06598bb8ff23ae67284d8f48996e953c553d
Victor Huynh <45438361+victor-h-huynh@users.noreply.github.com>
2020-12-05 14:26:34 -0500

    .

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -202,12 +202,12 @@
 // Coding Challenge #2
 ///////////////////////////////////////////////////////////////////////////////////////
 
-const markMass = 78;
-const markHeight = 1.69;
-const johnMass = 92;
-const johnHeight = 1.95;
-const markBMI = markMass / (markHeight * markHeight);
-const johnBMI = johnMass / (johnHeight * johnHeight)
+// const markMass = 78;
+// const markHeight = 1.69;
+// const johnMass = 92;
+// const johnHeight = 1.95;
+// const markBMI = markMass / (markHeight * markHeight);
+// const johnBMI = johnMass / (johnHeight * johnHeight)
 
 // console.log(markBMI);
 // console.log(johnBMI)
@@ -225,9 +225,9 @@ const johnBMI = johnMass / (johnHeight * johnHeight)
 // console.log(johnBMI)
 // console.log(markHigherBMI)
 
-if (markBMI > johnBMI) {
-    console.log(`Mark's BMI ${markBMI}is higher than John's!${johnBMI}`)
+// if (markBMI > johnBMI) {
+//     console.log(`Mark's BMI (${markBMI}) is higher than John's! (${johnBMI})`)
     
-} else {
-    console.log(`John's BMI ${johnBMI}is higher than Mark's!${markBMI}`)
-}
+// } else {
+//     console.log(`John's BMI (${johnBMI}) is higher than Mark's!(${markBMI})`)
+// }
