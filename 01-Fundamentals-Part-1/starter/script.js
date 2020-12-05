80171ef81152f64a7b93d1a8d9ebc233de9f3667
Victor Huynh <45438361+victor-h-huynh@users.noreply.github.com>
2020-12-05 14:38:44 -0500

    type-conversation-coercion

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -235,3 +235,15 @@
 ///////////////////////////////////////////////////////////////////////////////////////
 // Type Conversation and Coercion
 ///////////////////////////////////////////////////////////////////////////////////////
+
+// type converstion
+const inputYear = '1991';
+console.log(Number(inputYear), inputYear);
+console.log(Number(inputYear) + 18);
+
+console.log(Number('Jonas'))
+console.log(typeof NaN);
+
+console.log(String(23), 23);
+
+// type coercion
