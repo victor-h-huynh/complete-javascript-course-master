a96ebea2a3d6a1499600b2ff84595b2411f98953
Victor Huynh <45438361+victor-h-huynh@users.noreply.github.com>
2020-12-05 15:55:41 -0500

    truth-falsy-values

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -236,19 +236,23 @@
 // Type Conversation and Coercion
 ///////////////////////////////////////////////////////////////////////////////////////
 
-// type converstion
-const inputYear = '1991';
-console.log(Number(inputYear), inputYear);
-console.log(Number(inputYear) + 18);
+// // type converstion
+// const inputYear = '1991';
+// console.log(Number(inputYear), inputYear);
+// console.log(Number(inputYear) + 18);
 
-console.log(Number('Jonas'))
-console.log(typeof NaN);
+// console.log(Number('Jonas'))
+// console.log(typeof NaN);
 
-console.log(String(23), 23);
+// console.log(String(23), 23);
 
-// type coercion
-console.log('I am ' + 23 + ' years old');
-console.log('23' - '10' - 3);
-console.log('23' * '2');
-console.log('23' / '2');
-console.log('23' > '18');
+// // type coercion
+// console.log('I am ' + 23 + ' years old');
+// console.log('23' - '10' - 3);
+// console.log('23' * '2');
+// console.log('23' / '2');
+// console.log('23' > '18');
+
+///////////////////////////////////////////////////////////////////////////////////////
+// Truthy and Falsy Values
+///////////////////////////////////////////////////////////////////////////////////////
