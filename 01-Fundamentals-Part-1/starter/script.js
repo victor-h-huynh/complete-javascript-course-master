445336142d04e5973d859d42053de8ba1fa5f855
Victor Huynh <45438361+victor-h-huynh@users.noreply.github.com>
2020-12-06 20:37:37 -0500

    switch-statements

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -353,26 +353,43 @@
 // The switch statement
 ///////////////////////////////////////////////////////////////////////////////////////
 
-const day = 'monday';
-switch(day) {
-    case 'monday': // day === 'monday'
-        console.log('Plan course structure');
-        console.log('Go to coding meetup');
-        break;
-    case 'tuesday':
-        console.log('Prepare theory videos');
-        break;
-    case 'wednesday':
-    case 'thursday': 
-        console.log('Write code examples');
-        break;
-    case 'friday': 
-        console.log('Record videos');
-        break;
-    case 'saturday':
-    case 'sunday':
-        console.log('Enjoy the weekend :D');
-        break;
-    default:
-        console.log('Not a valid day!');
-}
+// const day = 'monday';
+// switch(day) {
+//     case 'monday': // day === 'monday'
+//         console.log('Plan course structure');
+//         console.log('Go to coding meetup');
+//         break;
+//     case 'tuesday':
+//         console.log('Prepare theory videos');
+//         break;
+//     case 'wednesday':
+//     case 'thursday': 
+//         console.log('Write code examples');
+//         break;
+//     case 'friday': 
+//         console.log('Record videos');
+//         break;
+//     case 'saturday':
+//     case 'sunday':
+//         console.log('Enjoy the weekend :D');
+//         break;
+//     default:
+//         console.log('Not a valid day!');
+// }
+
+// const today = 'monday'
+
+// if(today === 'monday') {
+//     console.log('Plan course structure');
+//     console.log('Go to coding meetup');
+// } else if(today === 'tuesday') {
+//     console.log('Prepare theory videos');
+// } else if(today === 'wednesday' || today === 'thursday') {
+//     console.log('Write code examples');
+// } else if(today === 'friday') {
+//     console.log('Record videos');
+// } else if (today === 'saturday' || today === 'sunday') {
+//     console.log('Enjoy the weekend :D');
+// } else {
+//     console.log('Not a valid day!');
+// }
