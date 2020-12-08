4087ac9514553629074a258d37c49b0edff04752
Victor Huynh <45438361+victor-h-huynh@users.noreply.github.com>
2020-12-08 09:58:35 -0500

    coding-challenge-4

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -419,7 +419,7 @@
 // Coding Challenge #4
 ///////////////////////////////////////////////////////////////////////////////////////
 
-const bill = 275
-const tip = bill >= 50 && bill <= 300 ? 0.15 * bill : 0.20 * bill;
+// const bill = 275
+// const tip = bill >= 50 && bill <= 300 ? 0.15 * bill : 0.20 * bill;
 
-console.log(`The bill was ${bill}, the tip was ${tip}, and the total value ${bill + tip}`)
+// console.log(`The bill was ${bill}, the tip was ${tip}, and the total value ${bill + tip}`)
