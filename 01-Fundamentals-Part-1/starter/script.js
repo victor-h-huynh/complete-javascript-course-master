5113694bce18fe4082d7e6801be086345c83430f
Victor Huynh <45438361+victor-h-huynh@users.noreply.github.com>
2020-12-08 09:58:16 -0500

    coding-challenge-4

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -421,5 +421,5 @@
 
 const bill = 275
 const tip = bill >= 50 && bill <= 300 ? 0.15 * bill : 0.20 * bill;
-console.log(tip)
-console.log('Bill:', bill, 'Tip:', tip, 'finalValue', bill + tip);
+
+console.log(`The bill was ${bill}, the tip was ${tip}, and the total value ${bill + tip}`)
