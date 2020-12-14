1049554225cf6723eae65f8c8fd513d4824037e2
Victor Huynh <45438361+victor-h-huynh@users.noreply.github.com>
2020-12-14 18:04:59 -0500

    arrow-function

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -55,6 +55,12 @@
 //Arrow Functions
 ///////////////////////////////////////////////////////////////
 
+// Function expression
 const calcAge2 = function (birthYear) {
     return 2037 - birthYear;
 }
+
+// Arrow function
+const calcAge3 = birthYear => 2037 - birthYear;
+const age3 = calcAge3(1991);
+console.log(age3);
