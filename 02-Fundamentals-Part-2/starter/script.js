0c00be43dc471f3da033d0e816faa8e988108077
Victor Huynh <45438361+victor-h-huynh@users.noreply.github.com>
2020-12-13 20:34:07 -0500

    functions

---
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,16 @@
+'use strict';
+
+let hasDriversLicense = false;
+const passTest = true;
+
+if (passTest) hasDriversLicense = true;
+if (hasDriversLicense) console.log('I can drive :D');
+
+// const interface = 'Audio';
+// const private = 534;
+
+function logger() {
+    console.log('My name is Jonas');
+}
+
+logger();
