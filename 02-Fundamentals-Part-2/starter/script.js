6536ca380d7b9a58ad1beb7eace278818a9b2e8b
Victor Huynh <45438361+victor-h-huynh@users.noreply.github.com>
2020-12-14 18:12:39 -0500

    arrow-functions

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -55,12 +55,17 @@
 //Arrow Functions
 ///////////////////////////////////////////////////////////////
 
-// Function expression
-const calcAge2 = function (birthYear) {
-    return 2037 - birthYear;
-}
-
 // Arrow function
 const calcAge3 = birthYear => 2037 - birthYear;
 const age3 = calcAge3(1991);
 console.log(age3);
+
+const yearsUntilRetirement = (birthYear, firstName) => {
+    const age = 2037 - birthYear;
+    const retirement = 65 - age;
+    // return retirement;
+    return `${firstName} retires in ${retirement} years`
+}
+
+console.log(yearsUntilRetirement(1991, 'Jonas'));
+console.log(yearsUntilRetirement(1980, 'Bob'));
