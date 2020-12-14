ca62773c174631007da89dcd98f540f0105188fc
Victor Huynh <45438361+victor-h-huynh@users.noreply.github.com>
2020-12-14 17:56:59 -0500

    function-declaration-expression

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -28,3 +28,25 @@
 
 // const appleOrangeJuice = fruitProcessor(2, 4);
 // console.log(appleOrangeJuice);
+
+
+////////////////////////////////////////////////////////////////
+//Function declaration vs Function Expression
+///////////////////////////////////////////////////////////////
+
+// Function declaration
+// function calcAge1(birthYear) {
+//     return 2037 - birthYear
+// }
+
+// const age1 = calcAge1(1991);
+// console.log(age1);
+
+// // Function expression
+// const calcAge2 = function (birthYear) {
+//     return 2037 - birthYear;
+// }
+
+// const age2 = calcAge2(1991);
+
+// console.log(age1, age2);
