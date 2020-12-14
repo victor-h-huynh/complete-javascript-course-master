9952f39ca1df3db8f33870549f4d52761b0ed8d8
Victor Huynh <45438361+victor-h-huynh@users.noreply.github.com>
2020-12-14 17:36:00 -0500

    function-declarations-expressions

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -1,30 +1,30 @@
 'use strict';
 
-let hasDriversLicense = false;
-const passTest = true;
+// let hasDriversLicense = false;
+// const passTest = true;
 
-if (passTest) hasDriversLicense = true;
-if (hasDriversLicense) console.log('I can drive :D');
+// if (passTest) hasDriversLicense = true;
+// if (hasDriversLicense) console.log('I can drive :D');
 
-// const interface = 'Audio';
-// const private = 534;
+// // const interface = 'Audio';
+// // const private = 534;
 
-function logger() {
-    console.log('My name is Jonas');
-}
+// function logger() {
+//     console.log('My name is Jonas');
+// }
 
-// calling / running / invoking function
-logger();
-logger();
-logger();
+// // calling / running / invoking function
+// logger();
+// logger();
+// logger();
 
-function fruitProcessor(apples, oranges) {
-    const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
-    return juice;
-}
+// function fruitProcessor(apples, oranges) {
+//     const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+//     return juice;
+// }
 
-const appleJuice = fruitProcessor(5, 0);
-console.log(appleJuice);
+// const appleJuice = fruitProcessor(5, 0);
+// console.log(appleJuice);
 
-const appleOrangeJuice = fruitProcessor(2, 4);
-console.log(appleOrangeJuice);
+// const appleOrangeJuice = fruitProcessor(2, 4);
+// console.log(appleOrangeJuice);
