914d6ad5696fe437da0b6fc83182b06d21d3090f
Victor Huynh <45438361+victor-h-huynh@users.noreply.github.com>
2020-12-14 20:42:14 -0500

    reviewing-functions

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -74,16 +74,40 @@
 // Functions Calling Other Functions
 ///////////////////////////////////////////////////////////////
 
-function cutFruitPieces(fruit) {
-    return fruit * 4;
+// function cutFruitPieces(fruit) {
+//     return fruit * 4;
+// }
+
+// function fruitProcessor(apples, oranges) {
+//     const applePieces = cutFruitPieces(apples);
+//     const orangePieces = cutFruitPieces(oranges);
+
+//     const juice = `Juice with ${applePieces} piece of apples and ${orangePieces} piece of oranges.`;
+//     return juice;
+// }
+
+// console.log(fruitProcessor(2, 3));
+
+////////////////////////////////////////////////////////////////
+// Reviewing Functions
+////////////////////////////////////////////////////////////////
+
+const calcAge = function(birthYear) {
+    return 2037 - birthYear;
 }
 
-function fruitProcessor(apples, oranges) {
-    const applePieces = cutFruitPieces(apples);
-    const orangePieces = cutFruitPieces(oranges);
+const yearsUntilRetirement = function (birthYear, firstName) {
+    const age = calcAge(birthYear)
+    const retirement = 65 - age;
 
-    const juice = `Juice with ${applePieces} piece of apples and ${orangePieces} piece of oranges.`;
-    return juice;
+    if(retirement > 0) {
+        console.log(`${firstName} retires in ${retirement} years`);
+        return retirement;
+    } else {
+        console.log(`${firstName} has already retired`);
+        return -1
+    }
 }
 
-console.log(fruitProcessor(2, 3));
+console.log(yearsUntilRetirement(1991, 'Jonas'));
+console.log(yearsUntilRetirement(1970, 'Mike'));
