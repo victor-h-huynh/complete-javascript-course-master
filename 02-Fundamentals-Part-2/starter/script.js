987bacbebc4b51a473c745df8941766f332de6a9
Victor Huynh <45438361+victor-h-huynh@users.noreply.github.com>
2020-12-15 14:19:06 -0500

    array-methods

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -182,3 +182,33 @@
 // Basic Array Operators(Methods)
 ////////////////////////////////////////////////////////////////
 
+const friends = ['Michael', 'Steven', 'Peter'];
+
+// Add elements
+friends.push('Jay'); // End
+console.log(friends);
+
+friends.unshift('John'); // Beginning
+console.log(friends);
+
+// Remove elements
+friends.pop(); // Last
+const popped = friends.pop();
+console.log(popped);
+console.log(friends);
+
+friends.shift(); // First
+console.log(friends);
+
+console.log(friends.indexOf('Steven'));
+console.log(friends.indexOf('Bob'));
+
+friends.push(23)
+console.log(friends)
+console.log(friends.includes('Steven'));
+console.log(friends.includes('Bob'));
+console.log(friends.includes(23));
+
+if (friends.includes('Steven')) {
+    console.log('You have a friend called Steven');
+}
