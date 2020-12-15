e087463b2ae12f61aa835cf7997c6de183f15e0f
Victor Huynh <45438361+victor-h-huynh@users.noreply.github.com>
2020-12-15 14:01:35 -0500

    basic-array-operators

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -35,6 +35,7 @@
 ///////////////////////////////////////////////////////////////
 
 // Function declaration
+
 // function calcAge1(birthYear) {
 //     return 2037 - birthYear
 // }
@@ -43,6 +44,7 @@
 // console.log(age1);
 
 // // Function expression
+
 // const calcAge2 = function (birthYear) {
 //     return 2037 - birthYear;
 // }
@@ -139,3 +141,44 @@
 ////////////////////////////////////////////////////////////////
 // Introduction to Arrays
 ////////////////////////////////////////////////////////////////
+
+// const friends = ['Michael', 'Steven', 'Peter'];
+// console.log(friends);
+
+// const y = new Array(1991, 1984, 2008, 2020);
+
+// console.log(friends[0]);
+// console.log(friends[2]);
+
+// console.log(friends.length);
+// console.log(friends[friends.length - 1]);
+
+// friends[2] = 'Jay';
+// console.log(friends);
+
+// const firstName = 'Jonas';
+// const jonas = [firstName, 'Schmedtmann', 2037 - 1991, 'teacher', friends];
+// console.log(jonas);
+// console.log(jonas.length)
+
+// //Exercise
+// function calcAge(birthYear) {
+//     return 2037 - birthYear
+// }
+
+// const years = [1990, 1967, 2002, 2010, 2018];
+
+// const age1 = calcAge(years[0]);
+// const age2 = calcAge(years[1]);
+// const age3 = calcAge(years[years.length - 1]);
+
+// console.log(age1, age2 ,age3);
+
+// const ages = [calcAge(years[0]), calcAge(years[1]), calcAge(years[years.length - 1])]
+// console.log(ages)
+
+
+////////////////////////////////////////////////////////////////
+// Basic Array Operators(Methods)
+////////////////////////////////////////////////////////////////
+
