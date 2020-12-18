333573627d38950854f97a7ccf67d590e2ee4b7d
Victor Huynh <45438361+victor-h-huynh@users.noreply.github.com>
2020-12-18 18:55:02 -0500

    object-methods

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -258,32 +258,40 @@
 // Dot vs Bracket Notation
 ////////////////////////////////////////////////////////////////
 
-const jonas = {
-    firstName: 'Jonas',
-    lastName: 'Schmedtmann',
-    age: 2037 - 1991,
-    job: 'teacher',
-    friends: ['Michael', 'Peter', 'Steven']
-};
+// const jonas = {
+//     firstName: 'Jonas',
+//     lastName: 'Schmedtmann',
+//     age: 2037 - 1991,
+//     job: 'teacher',
+//     friends: ['Michael', 'Peter', 'Steven']
+// };
 
-console.log(jonas);
+// console.log(jonas);
 
-console.log(jonas.lastName);
-console.log(jonas['lastName']);
+// console.log(jonas.lastName);
+// console.log(jonas['lastName']);
 
-const nameKey = 'Name';
-console.log(jonas['first' + nameKey]);
-console.log(jonas['last' + nameKey]);
+// const nameKey = 'Name';
+// console.log(jonas['first' + nameKey]);
+// console.log(jonas['last' + nameKey]);
 
-const interestedIn = prompt('What do you want to know about Jonas? Choose between firstName, lastName, age, job and friends');
+// const interestedIn = prompt('What do you want to know about Jonas? Choose between firstName, lastName, age, job and friends');
 
 
-if(jonas[interestedIn]) {
-    console.log(jonas[interestedIn]);
-} else {
-    console.log('Wrong request! Choose between firstName, lastName, age, job and friends');
-}
+// if(jonas[interestedIn]) {
+//     console.log(jonas[interestedIn]);
+// } else {
+//     console.log('Wrong request! Choose between firstName, lastName, age, job and friends');
+// }
 
-jonas.location = 'Portugal';
-jonas['twitter'] = '@jonasschemedtman';
-console.log(jonas);
+// jonas.location = 'Portugal';
+// jonas['twitter'] = '@jonasschemedtman';
+// console.log(jonas);
+
+// // Challenge
+// console.log(`${jonas['firstName']} has ${jonas.friends.length} friends and his best friend is named ${jonas['friends'][0]}`);
+
+////////////////////////////////////////////////////////////////
+// Object Methods
+////////////////////////////////////////////////////////////////
+
