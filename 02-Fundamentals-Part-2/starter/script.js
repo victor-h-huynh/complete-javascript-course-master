c01e0468453d255eafdc63059c96e149ec7d2337
Victor Huynh <45438361+victor-h-huynh@users.noreply.github.com>
2020-12-18 18:26:16 -0500

    dot-vs-bracket-notation

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -238,13 +238,25 @@
 // Objects
 ////////////////////////////////////////////////////////////////
 
-const jonasArray = [
-    'Jonas',
-    'Schedtmann',
-    2037 - 1991,
-    'teacher',
-    ['Michael', 'Peter', 'Steven']
-];
+// const jonasArray = [
+//     'Jonas',
+//     'Schedtmann',
+//     2037 - 1991,
+//     'teacher',
+//     ['Michael', 'Peter', 'Steven']
+// ];
+
+// const jonas = {
+//     firstName: 'Jonas',
+//     lastName: 'Schmedtmann',
+//     age: 2037 - 1991,
+//     job: 'teacher',
+//     friends: ['Michael', 'Peter', 'Steven']
+// };
+
+////////////////////////////////////////////////////////////////
+// Dot vs Bracket Notation
+////////////////////////////////////////////////////////////////
 
 const jonas = {
     firstName: 'Jonas',
@@ -253,3 +265,25 @@ const jonas = {
     job: 'teacher',
     friends: ['Michael', 'Peter', 'Steven']
 };
+
+console.log(jonas);
+
+console.log(jonas.lastName);
+console.log(jonas['lastName']);
+
+const nameKey = 'Name';
+console.log(jonas['first' + nameKey]);
+console.log(jonas['last' + nameKey]);
+
+const interestedIn = prompt('What do you want to know about Jonas? Choose between firstName, lastName, age, job and friends');
+
+
+if(jonas[interestedIn]) {
+    console.log(jonas[interestedIn]);
+} else {
+    console.log('Wrong request! Choose between firstName, lastName, age, job and friends');
+}
+
+jonas.location = 'Portugal';
+jonas['twitter'] = '@jonasschemedtman';
+console.log(jonas);
