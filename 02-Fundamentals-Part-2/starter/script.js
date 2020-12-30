d16aaf915f1ec7c69d783a793892ae8248302df2
Victor Huynh <45438361+victor-h-huynh@users.noreply.github.com>
2020-12-29 20:31:35 -0500

    looping-arrays-breaking-continuing

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -419,3 +419,18 @@ for (let i = 0; i < years.length; i++) {
 console.log(ages);
 
 // continue and break
+console.log('--- ONLY STRING ---')
+for(let i = 0; i < jonas.length; i++) {
+    if(typeof jonas[i] !== 'string') {
+        continue; 
+    }
+    console.log(jonas[i], typeof jonas[i])
+}
+
+console.log('--- BREAK WITH NUMBER ---')
+for(let i = 0; i < jonas.length; i++) {
+    if(typeof jonas[i] === 'number') {
+        break;
+    }
+    console.log(jonas[i], typeof jonas[i])
+}
