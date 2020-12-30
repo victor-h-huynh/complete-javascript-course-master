8c04eae441eb2bf30dee3dada1476083085e61a5
Victor Huynh <45438361+victor-h-huynh@users.noreply.github.com>
2020-12-29 21:05:09 -0500

    looping-backwards-loops-in-loops

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -439,22 +439,22 @@
 // Looping Backwards and Loops in Loops
 ////////////////////////////////////////////////////////////////
 
-const jonas = [
-    'Jonas',
-    'Schedtmann',
-    2037 - 1991,
-    'teacher',
-    ['Michael', 'Peter', 'Steven'],
-];
+// const jonas = [
+//     'Jonas',
+//     'Schedtmann',
+//     2037 - 1991,
+//     'teacher',
+//     ['Michael', 'Peter', 'Steven'],
+// ];
 
-for (let i = jonas.length - 1; i >= 0; i--) {
-    console.log(i, jonas[i]);
-}
+// for (let i = jonas.length - 1; i >= 0; i--) {
+//     console.log(i, jonas[i]);
+// }
 
-for (let exercise = 1; exercise < 4; exercise++) {
-    console.log(`----- Starting exercise ${exercise}`);
+// for (let exercise = 1; exercise < 4; exercise++) {
+//     console.log(`----- Starting exercise ${exercise}`);
 
-    for (let rep = 1; rep < 6; rep++) {
-        console.log(`Exercise ${exercise}: Lifting weight repetition ${rep}`)
-    }
-}
+//     for (let rep = 1; rep < 6; rep++) {
+//         console.log(`Exercise ${exercise}: Lifting weight repetition ${rep}`)
+//     }
+// }
