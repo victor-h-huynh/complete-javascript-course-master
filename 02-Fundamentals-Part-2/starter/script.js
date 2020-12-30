5a5cae9c9a742f91c27a928cb2fffec555e51f16
Victor Huynh <45438361+victor-h-huynh@users.noreply.github.com>
2020-12-29 20:03:37 -0500

    continue-break

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -382,15 +382,40 @@
 //     console.log(`Lifting weights reptition ${rep}`);
 // }
 
-// const jonasArray = [
-//     'Jonas',
-//     'Schedtmann',
-//     2037 - 1991,
-//     'teacher',
-//     ['Michael', 'Peter', 'Steven']
-// ];
-
 ////////////////////////////////////////////////////////////////
 // Looping Arrays, Breaking And Continuing
 ////////////////////////////////////////////////////////////////
 
+const jonas = [
+    'Jonas',
+    'Schedtmann',
+    2037 - 1991,
+    'teacher',
+    ['Michael', 'Peter', 'Steven'],
+    true
+];
+
+const types = [];
+
+for(let i = 0; i < jonas.length; i++) {
+    // Reading from jonas array
+    console.log(jonas[i], typeof jonas[i])
+
+    // Filling types array
+    // types[i] = typeof jonas[i]
+
+    types.push(typeof jonas[i])
+}
+
+console.log(types);
+
+const years = [1991, 2007, 1969, 2020];
+const ages = [];
+
+for (let i = 0; i < years.length; i++) {
+    ages.push(2037 - years[i]);
+}
+
+console.log(ages);
+
+// continue and break
