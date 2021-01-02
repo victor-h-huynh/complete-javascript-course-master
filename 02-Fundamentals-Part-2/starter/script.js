2262922ad3dec68dea793a22050b6f68809a9b95
Victor Huynh <45438361+victor-h-huynh@users.noreply.github.com>
2021-01-02 17:38:30 -0500

    the-while-loop

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -481,7 +481,12 @@
 // The while loop
 ////////////////////////////////////////////////////////////////
 
-for(let rep = 1; rep <= 10; rep++) {
-    console.log(`Lifting weights reptition ${rep}`);
-}
+// for(let rep = 1; rep <= 10; rep++) {
+//     console.log(`Lifting weights reptition ${rep}`);
+// }
 
+let rep = 1;
+while(rep <= 10) {
+    console.log(`WHILE: Lifting weights reptition ${rep}`);
+    rep++;
+}
