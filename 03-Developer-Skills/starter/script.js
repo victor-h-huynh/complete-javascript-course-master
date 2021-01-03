3b99535296faea52a21d313c09e720ad49523844
Victor Huynh <45438361+victor-h-huynh@users.noreply.github.com>
2021-01-03 17:35:33 -0500

    setting-up-prettier-vs-code

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -1,5 +1,9 @@
 // Remember, we're gonna use strict mode in all scripts now!
-"use strict";
+'use strict';
 
-const x = 23;
+const x = '23';
 if (x === 23) console.log(23);
+
+const calcAge = birthYear => 2037 - birthYear;
+
+console.log(x);
