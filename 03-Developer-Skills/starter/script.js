92c42e4ca57af91680c6660a8eb9177cf8ae2b92
Victor Huynh <45438361+victor-h-huynh@users.noreply.github.com>
2021-01-11 11:14:40 -0500

    using-google-stackoverflow-mdn

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -1,11 +1,21 @@
 // Remember, we're gonna use strict mode in all scripts now!
 'use strict';
 
-const x = '23';
-if (x === 23) console.log(23);
+// const x = '23';
+// if (x === 23) console.log(23);
 
-const calcAge = birthYear => 2037 - birthYear;
+// const calcAge = birthYear => 2037 - birthYear;
 
-console.log(x);
+// console.log(x);
 
-console.log(calcAge(1950));
+// console.log(calcAge(1950));
+
+// PROBLEM:
+// We work for a company building a smart home thermometer. Our most recent task is this: "Given an array of temperatures of one day, calculate the temperature amplitude. Keep in mind that sometimes there might be a sensor error."
+
+const temperatures = [3, -2, -6, -1, 'error', 9, 13, 17, 15, 14, 9, 5];
+
+// 1) Under the problem
+// - What is temperature amplitude? Answer: Difference between highest and lowest temp
+// - How to computer max and min temperatures?
+// - What's a sensor error? And what to do?
