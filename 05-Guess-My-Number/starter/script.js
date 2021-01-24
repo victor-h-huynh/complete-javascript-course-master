5d6a47e828418c6326dea5c3a2a7f7c4bd5bd7e4
Victor Huynh <45438361+victor-h-huynh@users.noreply.github.com>
2021-01-23 23:55:35 -0500

    manipulating-css-styles

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -12,10 +12,8 @@ document.querySelector('.guess').value = 23;
 console.log(document.querySelector('.guess').value);
 */
 
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
-
-document.querySelector('.number').textContent = secretNumber;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
@@ -27,6 +25,11 @@ document.querySelector('.check').addEventListener('click', function () {
     // When player wins
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = '🎉 Correct Number!';
+    document.querySelector('.number').textContent = secretNumber;
+
+    document.querySelector('body').style.backgroundColor = '#60b347';
+
+    document.querySelector('.number').style.width = '30rem';
     // When guess is too high
   } else if (guess > secretNumber) {
     if (score > 1) {
@@ -48,4 +51,19 @@ document.querySelector('.check').addEventListener('click', function () {
       document.querySelector('.score').textContent = 0;
     }
   }
+});
+
+// Select the element with the 'again' class and attach a click event handler
+document.querySelector('.again').addEventListener('click', function () {
+  // Restore initial value of the score and secretNumber varibles
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  // Restore the initial conditions of the message, number, score and guess input field
+  document.querySelector('.message').textContent = 'Start guessing...';
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.guess').value = '';
+  // Restore the original background color (#222) and number width (15rem)
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
 });
