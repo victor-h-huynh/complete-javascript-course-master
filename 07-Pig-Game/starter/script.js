fa561f31cf6c0b028caa5f53cf0d92f38803883b
Victor Huynh <45438361+victor-h-huynh@users.noreply.github.com>
2021-02-13 22:31:30 -0500

    pig-game-plus-rule

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -13,15 +13,27 @@ const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 
-// Starting conditions
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add('hidden');
+let scores, currentScore, activePlayer, playing;
 
-const scores = [0, 0];
-let currentScore = 0;
-let activePlayer = 0;
-let playing = true;
+// Starting conditions
+const init = function() {
+  scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
+
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+
+  diceEl.classList.add('hidden');
+  player0El.classList.remove('player--winner');
+  player1El.classList.remove('player--winner');
+  player0El.classList.add('player--active');
+  player1El.classList.remove('player--active');
+}
+init();
 
 const switchPlayer = function() {
   document.getElementById(`current--${activePlayer}`).textContent = 0;
@@ -75,18 +87,4 @@ if(playing) {
   }
 })
 
-btnNew.addEventListener('click', function() {
-  scores[0] = 0
-  scores[1] = 0
-  score0El.textContent = 0;
-  score1El.textContent = 0;
-  diceEl.classList.add('hidden');
-  currentScore = 0;
-  current0El.textContent = 0;
-  current1El.textContent = 0;
-  playing = true;
-  document.querySelector(`.player--${activePlayer}`).classList.remove('player--active')
-  document.querySelector(`.player--${activePlayer}`).classList.remove('player--winner')
-  document.querySelector(".player--0").classList.add('player--active')
-  activePlayer = 0;
-})
+btnNew.addEventListener('click', init)
