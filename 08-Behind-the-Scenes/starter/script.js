c51e40c4e365fc46967d434b97fa59f4d7d6fc1d
Victor Huynh <victor.h.huynh@gmail.com>
2021-06-01 12:52:04 -0400

    scoping-in-practice

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,3 +1,10 @@
 'use strict';
 
-console.log('hello');
+function calcAge(birthYear) {
+    const age = 2037 - birthYear;
+    console.log(firstName);
+    return age;
+}
+
+const firstName = 'Jonas';
+calcAge(1991);
