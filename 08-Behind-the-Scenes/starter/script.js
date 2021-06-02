9477a2d6dd266f69ffad49485c36d5717ebb5e56
Victor Huynh <victor.h.huynh@gmail.com>
2021-06-02 11:42:24 -0400

    hoisting-and-tdz

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -39,4 +39,37 @@
 
 /// Hoisting and TDZ in Practice
 
-var me = 'Jona' 
+// Variables
+console.log(me);
+// console.log(job);
+// console.log(year);
+
+var me = 'Jonas';
+let job = 'teacher';
+const year = 1991; 
+
+// Functions
+console.log(addDecl(2,3));
+// console.log(addExpr(2, 3));
+console.log(addArrow);
+// console.log(addArrow(2, 3));
+
+function addDecl(a, b) {
+    return a  + b;
+}
+
+const addExpr = function(a, b) {
+    return a + b;
+}
+
+var addArrow = (a, b) => a + b;
+
+// Example
+
+if(!numProducts) deleteShoppingCart();
+
+var numProducts = 10;
+
+function deleteShoppingCart() {
+    console.log('All products deleted!')
+}
