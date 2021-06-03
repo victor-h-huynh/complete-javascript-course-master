136a9a3b62fdce5ff403a43804c2028fb7ccdea6
Victor Huynh <victor.h.huynh@gmail.com>
2021-06-03 13:36:09 -0400

    this-keyword-in-practice

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -39,41 +39,70 @@
 
 /// Hoisting and TDZ in Practice
 
-// Variables
-console.log(me);
-// console.log(job);
-// console.log(year);
+// // Variables
+// console.log(me);
+// // console.log(job);
+// // console.log(year);
 
-var me = 'Jonas';
-let job = 'teacher';
-const year = 1991; 
+// var me = 'Jonas';
+// let job = 'teacher';
+// const year = 1991; 
 
-// Functions
-console.log(addDecl(2,3));
-// console.log(addExpr(2, 3));
-console.log(addArrow);
-// console.log(addArrow(2, 3));
+// // Functions
+// console.log(addDecl(2,3));
+// // console.log(addExpr(2, 3));
+// console.log(addArrow);
+// // console.log(addArrow(2, 3));
 
-function addDecl(a, b) {
-    return a  + b;
+// function addDecl(a, b) {
+//     return a  + b;
+// }
+
+// const addExpr = function(a, b) {
+//     return a + b;
+// }
+
+// var addArrow = (a, b) => a + b;
+
+// // Example
+// console.log('numProducts',numProducts)
+// if(!numProducts) deleteShoppingCart();
+
+// var numProducts = 10;
+
+// function deleteShoppingCart() {
+//     console.log('All products deleted!')
+// }
+
+// var x = 1;
+// let y = 2;
+// const z = 3;
+
+console.log(this);
+
+const calcAge = function(birthYear) {
+    console.log(2037 - birthYear);
+    console.log(this);
 }
+calcAge(1991);
 
-const addExpr = function(a, b) {
-    return a + b;
+const calcAgeArrow = birthYear => {
+    console.log(2037 - birthYear);
+    console.log(this);
 }
+calcAgeArrow(1980);
 
-var addArrow = (a, b) => a + b;
-
-// Example
-console.log('numProducts',numProducts)
-if(!numProducts) deleteShoppingCart();
-
-var numProducts = 10;
-
-function deleteShoppingCart() {
-    console.log('All products deleted!')
+const jonas = {
+    year: 1991,
+    calcAge: function() {
+        console.log(this);
+        console.log(2037 - this.year);
+    }
 }
+jonas.calcAge()
 
-var x = 1;
-let y = 2;
-const z = 3;
+const matilda = {
+    year: 2017,
+};
+
+matilda.calcAge = jonas.calcAge
