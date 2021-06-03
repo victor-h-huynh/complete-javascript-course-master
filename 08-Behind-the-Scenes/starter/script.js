47ab715ad4b2b9a5782c491b550a77e4de7dba84
Victor Huynh <victor.h.huynh@gmail.com>
2021-06-03 14:24:44 -0400

    regular-vs-arrow-function

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -80,33 +80,46 @@
 
 // console.log(this);
 
-const calcAge = function(birthYear) {
-    console.log(2037 - birthYear);
-    // console.log(this);
-}
-calcAge(1991);
+// const calcAge = function(birthYear) {
+//     console.log(2037 - birthYear);
+//     // console.log(this);
+// }
+// calcAge(1991);
 
-const calcAgeArrow = birthYear => {
-    console.log(2037 - birthYear);
-    // console.log(this);
-}
-calcAgeArrow(1980);
+// const calcAgeArrow = birthYear => {
+//     console.log(2037 - birthYear);
+//     // console.log(this);
+// }
+// calcAgeArrow(1980);
+
+// const jonas = {
+//     year: 1991,
+//     calcAge: function() {
+//         console.log(this);
+//         console.log(2037 - this.year);
+//     }
+// }
+// jonas.calcAge()
+
+// const matilda = {
+//     year: 2017,
+// };
+
+
+// matilda.calcAge = jonas.calcAge
+// matilda.calcAge();
+
+// const f = jonas.calcAge;
+// f();
 
 const jonas = {
+    firstName: 'Jonas',
     year: 1991,
     calcAge: function() {
         console.log(this);
         console.log(2037 - this.year);
-    }
-}
-jonas.calcAge()
+    },
 
-const matilda = {
-    year: 2017,
+    greet: () => console.log(`Hey ${this.firstName}`),
 };
-
-matilda.calcAge = jonas.calcAge
-matilda.calcAge();
-
-const f = jonas.calcAge;
-f();
+jonas.greet();
