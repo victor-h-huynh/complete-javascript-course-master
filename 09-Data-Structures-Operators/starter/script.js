9e1dde86a37ace224f56b8ad586b0c2e6ca5ea8b
Victor Huynh <victor.h.huynh@gmail.com>
2021-06-04 16:39:56 -0400

    destructuring-objects

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -23,7 +23,18 @@ const restaurant = {
   order: function (starterIndex, mainIndex) {
     return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
   },
+
+  orderDelivery: function({starterIndex, mainIndex, time, address}) {
+    console.log(`Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`)
+  },
 };
+
+restaurant.orderDelivery({
+  time: '22:30',
+  address: 'Via del Sole, 21',
+  mainIndex: 2,
+  starterIndex: 2,
+});
 
 const {name, openingHours, categories} = restaurant;
 console.log(name, openingHours, categories)
@@ -40,7 +51,18 @@ const { menu = [], starterMenu: starters = []} = restaurant;
 console.log(menu, starters);
 
 // Mutating variables
+let a = 111;
+let b = 999;
+const obj = { a: 23, b: 7, c: 14 };
 
+({a, b} = obj);
+console.log(a, b)
+
+// Nested objects
+const {
+  fri: {open: o, close: c},
+} = openingHours;
+console.log(o, c);
 /////////////////////////////////////////////////////////
 // Destructuring Arrays
 
