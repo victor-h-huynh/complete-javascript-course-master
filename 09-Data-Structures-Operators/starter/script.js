5307bcf4f9157d9745b8ffb1fc9cc21d8ede8d15
Victor Huynh <victor.h.huynh@gmail.com>
2021-06-04 16:43:39 -0400

    destructuring-objects

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -24,7 +24,7 @@ const restaurant = {
     return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
   },
 
-  orderDelivery: function({starterIndex, mainIndex, time, address}) {
+  orderDelivery: function({starterIndex = 1, mainIndex = 0, time = '20:00', address}) {
     console.log(`Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`)
   },
 };
@@ -34,6 +34,11 @@ restaurant.orderDelivery({
   address: 'Via del Sole, 21',
   mainIndex: 2,
   starterIndex: 2,
+});
+
+restaurant.orderDelivery({
+  address: 'Via del Sole, 21',
+  starterIndex: 1, 
 });
 
 const {name, openingHours, categories} = restaurant;
