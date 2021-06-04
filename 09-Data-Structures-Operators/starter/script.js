a47f8a6b66e447ec184ae00ae6e1dc4d6339a699
Victor Huynh <victor.h.huynh@gmail.com>
2021-06-04 16:02:03 -0400

    destructuring-objects

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -6,10 +6,6 @@ const restaurant = {
   categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
-
-  order: function (starterIndex, mainIndex) {
-    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
-  },
   openingHours: {
     thu: {
       open: 12,
@@ -24,39 +20,61 @@ const restaurant = {
       close: 24,
     },
   },
+  order: function (starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
 };
 
-const arr = [2,3,4];
-const a = arr[0];
-const b = arr[1];
-const c = arr[2];
+const {name, openingHours, categories} = restaurant;
+console.log(name, openingHours, categories)
 
-const [x, y, z] = arr;
-console.log(x, y, z)
-console.log(arr);
-
-let [main, , secondary] = restaurant.categories;
-console.log(main, secondary);
-
-// Switching variables
-// const temp = main;
-// main = secondary;
-// secondary = temp;
-// console.log(main, secondary);
-
-[main, secondary] = [secondary, main];
-console.log(main, secondary);
-
-// Receive 2 return values from a function
-const [starter, mainCourse] = restaurant.order(2, 0);
-console.log(starter, mainCourse);
-
-// Nested destructuring
-const nested = [2, 4, [5, 6]];
-// const [i, ,j] = nested;
-const [i, , [j, k]] = nested;
-console.log(i, j, k)
+const {
+  name: restaurantName, 
+  openingHours: hours, 
+  categories: tags
+} = restaurant;
+console.log(restaurantName, hours, tags)
 
 // Default values
-const [p = 1, q = 1, r = 1] = [8]
-console.log(p, q, r);
+const { menu = [], starterMenu: starters = []} = restaurant;
+console.log(menu, starters);
+
+// Mutating variables
+
+/////////////////////////////////////////////////////////
+// Destructuring Arrays
+
+// const arr = [2,3,4];
+// const a = arr[0];
+// const b = arr[1];
+// const c = arr[2];
+
+// const [x, y, z] = arr;
+// console.log(x, y, z)
+// console.log(arr);
+
+// let [main, , secondary] = restaurant.categories;
+// console.log(main, secondary);
+
+// // Switching variables
+// // const temp = main;
+// // main = secondary;
+// // secondary = temp;
+// // console.log(main, secondary);
+
+// [main, secondary] = [secondary, main];
+// console.log(main, secondary);
+
+// // Receive 2 return values from a function
+// const [starter, mainCourse] = restaurant.order(2, 0);
+// console.log(starter, mainCourse);
+
+// // Nested destructuring
+// const nested = [2, 4, [5, 6]];
+// // const [i, ,j] = nested;
+// const [i, , [j, k]] = nested;
+// console.log(i, j, k)
+
+// // Default values
+// const [p = 1, q = 1, r = 1] = [8]
+// console.log(p, q, r);
