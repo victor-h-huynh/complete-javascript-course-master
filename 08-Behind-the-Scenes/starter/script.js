15a08e9a158626d2b675923d05ab80c8c088d1b7
Victor Huynh <victor.h.huynh@gmail.com>
2021-06-04 12:51:54 -0400

    primitives-vs-reference-types

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -159,15 +159,18 @@
 // };
 // addArrow(2, 5, 8);
 
-let age = 30;
-let oldAge = age;
-age = 31;
-console.log(age);
-console.log(oldAge);
+// let age = 30;
+// let oldAge = age;
+// age = 31;
+// console.log(age);
+// console.log(oldAge);
 
-const me = {
-    name: 'Jonas',
-    age: 30,
-}
+// const me = {
+//     name: 'Jonas',
+//     age: 30,
+// }
 
-const friend = me;
+// const friend = me;
+// friend.age = 27;
+// console.log('Friend', friend);
+// console.log('Me', me);
