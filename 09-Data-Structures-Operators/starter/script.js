f0656ab74f4e821ab007a113c9e4a4f75b5a8a77
Victor Huynh <victor.h.huynh@gmail.com>
2021-06-07 12:45:52 -0400

    rest-pattern-parameters

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -61,8 +61,18 @@ const { sat, ...weekdays } = restaurant.openingHours;
 console.log(weekdays);
 
 // 2) Functions
-const add = function () {};
+const add = function (...numbers) {
+  let sum = 0;
+  for (let i = 0; i < numbers.length; i++) sum += numbers[i];
+  console.log(sum);
+};
 
+add(2, 3);
+add(5, 3, 7, 2);
+add(8, 2, 5, 3, 2, 1, 4);
+
+const x = [23, 5, 7];
+add(...x);
 /////////////////////////////////////////////////////////
 // The Spread Operator (...)
 
