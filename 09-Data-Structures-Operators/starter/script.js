ba062f69de769184876d72850b746f40448706a7
Victor Huynh <victor.h.huynh@gmail.com>
2021-06-10 15:48:34 -0400

    coding-challenge-1

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -275,75 +275,86 @@ TEST DATA FOR 6: Use players 'Davies', 'Muller', 'Lewandowski' and 'Kimmich'. Th
 GOOD LUCK 😀
 */
 
-const game = {
-  team1: 'Bayern Munich',
-  team2: 'Borrussia Dortmund',
-  players: [
-    [
-      'Neuer',
-      'Pavard',
-      'Martinez',
-      'Alaba',
-      'Davies',
-      'Kimmich',
-      'Goretzka',
-      'Coman',
-      'Muller',
-      'Gnarby',
-      'Lewandowski',
-    ],
-    [
-      'Burki',
-      'Schulz',
-      'Hummels',
-      'Akanji',
-      'Hakimi',
-      'Weigl',
-      'Witsel',
-      'Hazard',
-      'Brandt',
-      'Sancho',
-      'Gotze',
-    ],
-  ],
-  score: '4:0',
-  scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
-  date: 'Nov 9th, 2037',
-  odds: {
-    team1: 1.33,
-    x: 3.25,
-    team2: 6.5,
-  },
-};
+// const game = {
+//   team1: 'Bayern Munich',
+//   team2: 'Borrussia Dortmund',
+//   players: [
+//     [
+//       'Neuer',
+//       'Pavard',
+//       'Martinez',
+//       'Alaba',
+//       'Davies',
+//       'Kimmich',
+//       'Goretzka',
+//       'Coman',
+//       'Muller',
+//       'Gnarby',
+//       'Lewandowski',
+//     ],
+//     [
+//       'Burki',
+//       'Schulz',
+//       'Hummels',
+//       'Akanji',
+//       'Hakimi',
+//       'Weigl',
+//       'Witsel',
+//       'Hazard',
+//       'Brandt',
+//       'Sancho',
+//       'Gotze',
+//     ],
+//   ],
+//   score: '4:0',
+//   scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+//   date: 'Nov 9th, 2037',
+//   odds: {
+//     team1: 1.33,
+//     x: 3.25,
+//     team2: 6.5,
+//   },
+// };
 
-const [players1, players2] = game.players;
+// const [players1, players2] = game.players;
 
-const [gk, ...fieldPlayers] = players1;
-const allPlayers = [...players1, ...players2];
-const players1Final = [...players1, 'Thiago', 'Coutinho', 'Perisic'];
-const {
-  odds: { team1, x: draw, team2 },
-} = game;
+// const [gk, ...fieldPlayers] = players1;
+// const allPlayers = [...players1, ...players2];
+// const players1Final = [...players1, 'Thiago', 'Coutinho', 'Perisic'];
+// const {
+//   odds: { team1, x: draw, team2 },
+// } = game;
 
-console.log(players1);
-console.log(players2);
-console.log(gk);
-console.log(fieldPlayers);
-console.log(allPlayers);
-console.log(players1Final);
-console.log(team1, draw, team2);
+// console.log(players1);
+// console.log(players2);
+// console.log(gk);
+// console.log(fieldPlayers);
+// console.log(allPlayers);
+// console.log(players1Final);
+// console.log(team1, draw, team2);
 
-function printGoals(player) {
-  let goals = 0;
-  for (let i = 0; i < game.scored.length; i++) {
-    if (game.scored[i] === player) goals++;
-  }
-  console.log(`${player} scored ${goals}`);
-}
+// const printGoals = function (...players) {
+//   console.log(players);
+//   console.log(`${players.length} goals were scored`);
+// };
 
-printGoals('Davies');
-printGoals('Muller');
-printGoals('Lewandowski');
-printGoals('Kimmich');
+// printGoals('Davies', 'Muller', 'Lewandowski', 'Kimmich');
+// printGoals('Davies', 'Muller');
+// printGoals(...game.scored);
 
-console.log(game.odds.team1 < game.odds.team2, 'team1 is more likely to win');
+// My wrong answer
+// function printGoals(player) {
+//   let goals = 0;
+//   for (let i = 0; i < game.scored.length; i++) {
+//     if (game.scored[i] === player) goals++;
+//   }
+//   console.log(`${player} scored ${goals}`);
+// }
+
+// printGoals('Davies');
+// printGoals('Muller');
+// printGoals('Lewandowski');
+// printGoals('Kimmich');
+
+// team1 < team2 && console.log('Team 1 is more likely to win');
+// team1 > team2 && console.log('Team 1 is more likely to win');
