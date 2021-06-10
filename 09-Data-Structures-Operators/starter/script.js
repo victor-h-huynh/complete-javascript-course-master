66576f4fe9a9b21b1cb36f5e691cfb2130865bb9
Victor Huynh <victor.h.huynh@gmail.com>
2021-06-10 12:42:13 -0400

    coding-challenge-1

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -264,7 +264,7 @@ Suppose we get data from a web service about a certain game (below). In this cha
 
 1. Create one player array for each team (variables 'players1' and 'players2')
 2. The first player in any player array is the goalkeeper and the others are field players. For Bayern Munich (team 1) create one variable ('gk') with the goalkeeper's name, and one array ('fieldPlayers') with all the remaining 10 field players
-3.  --> Create an array 'allPlayers' containing all players of both teams (22 players)
+3. Create an array 'allPlayers' containing all players of both teams (22 players)
 4. During the game, Bayern Munich (team 1) used 3 substitute players. So create a new array ('players1Final') containing all the original team1 players plus 'Thiago', 'Coutinho' and 'Perisic'
 5. Based on the game.odds object, create one variable for each odd (called 'team1', 'draw' and 'team2')
 6. Write a function ('printGoals') that receives an arbitrary number of player names (NOT an array) and prints each of them to the console, along with the number of goals that were scored in total (number of player names passed in)
@@ -316,10 +316,34 @@ const game = {
   },
 };
 
-const players1 = game.players[0];
-const players2 = game.players[1];
+const [players1, players2] = game.players;
+
 const [gk, ...fieldPlayers] = players1;
+const allPlayers = [...players1, ...players2];
+const players1Final = [...players1, 'Thiago', 'Coutinho', 'Perisic'];
+const {
+  odds: { team1, x: draw, team2 },
+} = game;
+
 console.log(players1);
 console.log(players2);
 console.log(gk);
 console.log(fieldPlayers);
+console.log(allPlayers);
+console.log(players1Final);
+console.log(team1, draw, team2);
+
+function printGoals(player) {
+  let goals = 0;
+  for (let i = 0; i < game.scored.length; i++) {
+    if (game.scored[i] === player) goals++;
+  }
+  console.log(`${player} scored ${goals}`);
+}
+
+printGoals('Davies');
+printGoals('Muller');
+printGoals('Lewandowski');
+printGoals('Kimmich');
+
+console.log(game.odds.team1 < game.odds.team2, 'team1 is more likely to win');
