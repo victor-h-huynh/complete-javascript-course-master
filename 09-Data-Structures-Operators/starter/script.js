bd5041580f23c849409f232e916a80d15c61b87f
Victor Huynh <victor.h.huynh@gmail.com>
2021-06-16 11:52:05 -0400

    looping-object

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -47,28 +47,28 @@ const restaurant = {
 };
 
 // Property NAMES
-const properties = Object.keys(openingHours);
-console.log(properties);
+// const properties = Object.keys(openingHours);
+// console.log(properties);
 
-let openStr = `We are open on ${properties.length} days: `;
+// let openStr = `We are open on ${properties.length} days: `;
 
-for (const day of properties) {
-  openStr += `${day}, `;
-}
-console.log(openStr);
+// for (const day of properties) {
+//   openStr += `${day}, `;
+// }
+// console.log(openStr);
 
-// Property VALUES
-const values = Object.values(openingHours);
-console.log(values);
+// // Property VALUES
+// const values = Object.values(openingHours);
+// console.log(values);
 
-// Entire Object
-const entries = Object.entries(openingHours);
-// console.log(entries);
+// // Entire Object
+// const entries = Object.entries(openingHours);
+// // console.log(entries);
 
-// [key, value]
-for (const [day, { open, close }] of entries) {
-  console.log(`On ${day} we open at ${open} and close at ${close}`);
-}
+// // [key, value]
+// for (const [day, { open, close }] of entries) {
+//   console.log(`On ${day} we open at ${open} and close at ${close}`);
+// }
 
 // for (const day of Object.keys(openingHours))
 
