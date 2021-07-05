4e82c9f7b824ec62d0ff18c1f6d8456e50e3211e
Victor Huynh <victor.h.huynh@gmail.com>
2021-07-05 10:46:54 -0400

    map-iteration

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -61,6 +61,17 @@ console.log(question);
 console.log(Object.entries(openingHours));
 const hoursMap = new Map(Object.entries(openingHours));
 console.log(hoursMap);
+
+// Quiz app
+console.log(question.get('question'));
+for (const [key, value] of question) {
+  if (typeof key === 'number');
+  console.log(`Answer ${key}: ${value}`);
+}
+const answer = Number(prompt('Your answer'));
+console.log(answer);
+
+console.log(question.get(question.get('correct') === answer));
 /////////////////////////////////////////////////////////
 // Maps: Fundamentals
 // const rest = new Map();
