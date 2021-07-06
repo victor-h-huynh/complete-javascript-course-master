27a462dfe3a2b291bea5f94133e02cce458354b3
Victor Huynh <victor.h.huynh@gmail.com>
2021-07-06 14:22:36 -0400

    coding-challenge-3

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -86,10 +86,11 @@ console.log(
   `An event happened, on average, every ${90 / gameEvents.size} minutes`
 );
 // 4.
-for (const [minute, event] of gameEvents) {
-  minute < 45
-    ? console.log(`[FIRST HALF] ${minute}: ${event}`)
-    : console.log(`[SECOND HALF] ${minute}: ${event}`);
+for (const [min, event] of gameEvents) {
+  const half = min <= 45 ? 'FIRST' : 'SECOND';
+  min < 45
+    ? console.log(`[${half} HALF] ${min}: ${event}`)
+    : console.log(`[${half} HALF] ${min}: ${event}`);
 }
 /////////////////////////////////////////////////////////
 // Maps: Iteration
