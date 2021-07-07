31d8c9b402b49499dfa097e45c28b7e3eae0260e
Victor Huynh <victor.h.huynh@gmail.com>
2021-07-07 16:21:49 -0400

    working-with-strings

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -92,7 +92,14 @@ if (plane.startsWith('Airbus') && plane.endsWith('neo')) {
 }
 
 // Practice exercise
-const checkBaggage = function (items) {};
+const checkBaggage = function (items) {
+  const baggage = items.toLowerCase();
+  if (baggage.includes('knife') || baggage.includes('gun')) {
+    console.log('You are NOT allowed on board');
+  } else {
+    console.log('Welcome aboard!');
+  }
+};
 checkBaggage('I have a laptop, some Food and a pocket Knife');
 checkBaggage('Socks and camera');
 checkBaggage('Got some snacks and a gun for protection');
