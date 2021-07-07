bd19d8f6d1e8089eb1f6cbd91f467dc3507c2b11
Victor Huynh <victor.h.huynh@gmail.com>
2021-07-07 15:53:25 -0400

    working-with-strings

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -46,48 +46,51 @@ const restaurant = {
   },
 };
 
-const airline = 'TAP Air Portugal';
-const plane = 'A320';
+///////////////////////////////////////
+// Working with Strings - Part 1
 
-console.log(plane[0]);
-console.log(plane[1]);
-console.log(plane[2]);
-console.log('B737'[0]);
+// const airline = 'TAP Air Portugal';
+// const plane = 'A320';
 
-console.log(airline.length);
-console.log('B737'.length);
+// console.log(plane[0]);
+// console.log(plane[1]);
+// console.log(plane[2]);
+// console.log('B737'[0]);
 
-console.log(airline.indexOf('r'));
-console.log(airline.lastIndexOf('r'));
-console.log(airline.indexOf('Portugal'));
+// console.log(airline.length);
+// console.log('B737'.length);
 
-console.log(airline.slice(4));
-console.log(airline.slice(4, 7));
+// console.log(airline.indexOf('r'));
+// console.log(airline.lastIndexOf('r'));
+// console.log(airline.indexOf('Portugal'));
 
-console.log(airline.slice(0, airline.indexOf(' ')));
-console.log(airline.slice(airline.lastIndexOf(' ') + 1));
+// console.log(airline.slice(4));
+// console.log(airline.slice(4, 7));
 
-console.log(airline.slice(-2));
-console.log(airline.slice(1, -1));
+// console.log(airline.slice(0, airline.indexOf(' ')));
+// console.log(airline.slice(airline.lastIndexOf(' ') + 1));
 
-const checkMiddleSeat = function (seat) {
-  // B and E are middle seats
-  const s = seat.slice(-1);
-  if (s === 'B' || s === 'E') {
-    console.log('You got the middle seat');
-  } else {
-    console.log('You got lucky!');
-  }
-};
+// console.log(airline.slice(-2));
+// console.log(airline.slice(1, -1));
 
-checkMiddleSeat('11B');
-checkMiddleSeat('23C');
-checkMiddleSeat('3E');
+// const checkMiddleSeat = function (seat) {
+//   // B and E are middle seats
+//   const s = seat.slice(-1);
+//   if (s === 'B' || s === 'E') {
+//     console.log('You got the middle seat');
+//   } else {
+//     console.log('You got lucky!');
+//   }
+// };
 
-console.log(new String('jonas'));
-console.log(typeof new String('jonas'));
+// checkMiddleSeat('11B');
+// checkMiddleSeat('23C');
+// checkMiddleSeat('3E');
 
-console.log(typeof new String('jonas').slice(1));
+// console.log(new String('jonas'));
+// console.log(typeof new String('jonas'));
+
+// console.log(typeof new String('jonas').slice(1));
 
 ///////////////////////////////////////
 // Coding Challenge #3
