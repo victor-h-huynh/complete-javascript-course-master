2a82d82715c25004bdf4d1d774c5ad450fa75d32
victor-h-huynh <victor.h.huynh@gmail.com>
2021-09-14 16:21:00 -0400

    working-with-strings-part-3

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -50,27 +50,52 @@ const restaurant = {
 // Working with Strings - Part 3
 
 // Split and join
-console.log('a+very+nice+string'.split('+'));
-console.log('Victor Huynh'.split(' '));
+// console.log('a+very+nice+string'.split('+'));
+// console.log('Victor Huynh'.split(' '));
 
-const [firstName, lastName] = 'Victor Huynh'.split(' ');
+// const [firstName, lastName] = 'Victor Huynh'.split(' ');
 
-const newName = ['Mr.', firstName, lastName.toUpperCase()].join(' ');
-console.log(newName);
+// const newName = ['Mr.', firstName, lastName.toUpperCase()].join(' ');
+// console.log(newName);
 
-const capitalizeName = function (name) {
-  const names = name.split(' ');
-  const namesUpper = [];
+// const capitalizeName = function (name) {
+//   const names = name.split(' ');
+//   const namesUpper = [];
 
-  for(const n of names) {
-    // namesUpper.push(n[0].toUpperCase() + n.slice(1));
-    namesUpper.push(n.replace(n[0], n[0].toUpperCase()));
-  }
-  console.log(namesUpper.join(' '));
-}
+//   for(const n of names) {
+//     // namesUpper.push(n[0].toUpperCase() + n.slice(1));
+//     namesUpper.push(n.replace(n[0], n[0].toUpperCase()));
+//   }
+//   console.log(namesUpper.join(' '));
+// }
 
-capitalizeName('jessica ann smith davis');
-capitalizeName('victor huynh');
+// capitalizeName('jessica ann smith davis');
+// capitalizeName('victor huynh');
+
+// // Padding
+// const message = 'Go to gate 23!';
+// console.log(message.padStart(20, '+').padEnd(30, '+'));
+// console.log('Jonas'.padStart(20, '+').padEnd(30, '+'));
+
+// const maskCreditCard = function(number) {
+//   const str = number + '';
+//   const last = str.slice(-4);
+//   return last.padStart(str.length, '*');
+// }
+
+// console.log(maskCreditCard(123456789));
+// console.log(maskCreditCard('123456789'));
+
+// // Repeat
+// const message2 = 'Bad weather... All Departures Delayed... ';
+// console.log(message2.repeat(5));
+
+// const planesInLine = function(n) {
+//   console.log(`There are ${n} planes in line ${'✈️'.repeat(n)}`);
+// }
+// planesInLine(5);
+// planesInLine(3);
+// planesInLine(12);
 
 ///////////////////////////////////////
 // Working with Strings - Part 2
