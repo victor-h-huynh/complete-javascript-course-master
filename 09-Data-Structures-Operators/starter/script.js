315cc4980484a262a435518593a1f04b3ae8776c
victor-h-huynh <victor.h.huynh@gmail.com>
2021-09-14 15:33:58 -0400

    working-with-strings-part-3

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -58,6 +58,20 @@ const [firstName, lastName] = 'Victor Huynh'.split(' ');
 const newName = ['Mr.', firstName, lastName.toUpperCase()].join(' ');
 console.log(newName);
 
+const capitalizeName = function (name) {
+  const names = name.split(' ');
+  const namesUpper = [];
+
+  for(const n of names) {
+    // namesUpper.push(n[0].toUpperCase() + n.slice(1));
+    namesUpper.push(n.replace(n[0], n[0].toUpperCase()));
+  }
+  console.log(namesUpper.join(' '));
+}
+
+capitalizeName('jessica ann smith davis');
+capitalizeName('victor huynh');
+
 ///////////////////////////////////////
 // Working with Strings - Part 2
 
