5f8fb710188a9f28a67120d675dcc87e508f983a
victor-h-huynh <victor.h.huynh@gmail.com>
2021-09-20 10:18:47 -0400

    coding-challenge-4

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -47,6 +47,57 @@ const restaurant = {
 };
 
 ///////////////////////////////////////
+// Coding Challenge #4
+
+/* 
+Write a program that receives a list of variable names written in underscore_case and convert them to camelCase.
+
+The input will come from a textarea inserted into the DOM (see code below), and conversion will happen when the button is pressed.
+
+THIS TEST DATA (pasted to textarea)
+underscore_case
+ first_name
+Some_Variable 
+  calculate_AGE
+delayed_departure
+
+SHOULD PRODUCE THIS OUTPUT (5 separate console.log outputs)
+underscoreCase      ✅
+firstName           ✅✅
+someVariable        ✅✅✅
+calculateAge        ✅✅✅✅
+delayedDeparture    ✅✅✅✅✅
+
+HINT 1: Remember which character defines a new line in the textarea 😉
+HINT 2: The solution only needs to work for a variable made out of 2 words, like a_b
+HINT 3: Start without worrying about the ✅. Tackle that only after you have the variable name conversion working 😉
+HINT 4: This challenge is difficult on purpose, so start watching the solution in case you're stuck. Then pause and continue!
+
+Afterwards, test with your own test data!
+
+GOOD LUCK 😀
+*/
+
+document.body.append(document.createElement('textarea'));
+document.body.append(document.createElement('button'));
+
+const button = document.querySelector('button');
+
+button.onclick = function convertToCamelCase () {
+  const text = document.querySelector('textarea').value;
+  let splitStr = text.toLowerCase().split('_');
+  let camelCase = splitStr[1].toUpperCase();
+  console.log(camelCase);
+
+}
+
+// const btn = document.querySelector('button');
+
+// btn.onClick = function() {
+//   console.log(text);
+// }
+
+///////////////////////////////////////
 // Working with Strings - Part 3
 
 // Split and join
