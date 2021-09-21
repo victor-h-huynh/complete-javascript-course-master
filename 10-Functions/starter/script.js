27ef90dde1879b6f9fab6aac558ab6c25f41cc30
victor-h-huynh <victor.h.huynh@gmail.com>
2021-09-21 18:11:24 -0400

    default-parameter

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -18,3 +18,7 @@ const createBooking = function(flightNum, numPassengers = 1, price = 199 * numPa
 
 createBooking('LH123');
 createBooking('LH123', 2, 800);
+createBooking('LH123', 2);
+createBooking('LH123', 5);
+
+createBooking('LH123', undefined, 1000);
