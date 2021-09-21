bbb77ea8add5a6362d7ac5c493b2ad78e7e308ce
victor-h-huynh <victor.h.huynh@gmail.com>
2021-09-21 17:28:24 -0400

    coding-challenge-4

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -59,7 +59,7 @@ const getCode = str => str.slice(0, 3).toUpperCase();
 
 for(const flight of flights.split('+')) {
   const [type, from, to, time] = flight.split(';');
-  const output = `${type.startsWith('_Delayed') ? '🔴' : ''}${type.replaceAll('_', ' ')} from ${getCode(from)} to ${getCode(to)} (${time.replace(':','h')})`
+  const output = `${type.startsWith('_Delayed') ? '🔴' : ''}${type.replaceAll('_', ' ')} from ${getCode(from)} to ${getCode(to)} (${time.replace(':','h')})`.padStart(44);
   console.log(output);
 }
 ///////////////////////////////////////
