a7cf2a59b051213992eec1a59703982744061714
victor-h-huynh <victor.h.huynh@gmail.com>
2021-09-21 18:06:45 -0400

    test

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,2 +1,18 @@
 'use strict';
-//
+
+const bookings = [];
+
+const createBooking = function(flightNum, numPassengers, price) {
+    numPassengers = numPassengers || 1;
+    price = price || 199;
+
+    const booking = {
+        flightNum,
+        numPassengers,
+        price
+    }
+    console.log(booking);
+    bookings.push(booking);
+}
+
+createBooking('LH123')
