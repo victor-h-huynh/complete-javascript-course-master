a9b621bf60edae82cf0720bb89e21b6e4869af44
victor-h-huynh <victor.h.huynh@gmail.com>
2021-09-22 18:05:37 -0400

    call-apply-methods

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -127,7 +127,7 @@ lufthansa.book(635, 'John Smith');
 console.log(lufthansa);
 
 const eurowings = {
-    name: 'Eurowings',
+    airline: 'Eurowings',
     iataCode: 'EW',
     bookings: [],
 };
@@ -137,5 +137,21 @@ const book = lufthansa.book;
 // Does NOT work
 // book(23, 'Sarah Williams');
 
+
+// Call method
 book.call(eurowings, 23, 'Sarah Williams');
 console.log(eurowings);
+
+book.call(lufthansa, 239, 'Mary Cooper');
+console.log(lufthansa);
+
+const swiss = {
+    airline: 'Swiss Air Lines',
+    iataCode: 'LX',
+    bookings: []
+};
+
+book.call(swiss, 583, 'Mary Cooper')
+console.log(swiss);
+
+// Apply method
