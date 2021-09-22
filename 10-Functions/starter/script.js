7f689391d4270f8a2860b2e4625ed33cb2f4461f
victor-h-huynh <victor.h.huynh@gmail.com>
2021-09-21 20:13:31 -0400

    first-class-higher-order-function

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -23,35 +23,35 @@
 
 // createBooking('LH123', undefined, 1000);
 
-const flight = 'LH234';
-const jonas = {
-    name: 'Jonas Schmedtmann',
-    passport: 24739479284
-}
+// const flight = 'LH234';
+// const jonas = {
+//     name: 'Jonas Schmedtmann',
+//     passport: 24739479284
+// }
 
-const checkIn = function(flightNum, passenger) {
-    flightNum = 'LH999';
-    passenger.name = 'Mr. ' + passenger.name;
+// const checkIn = function(flightNum, passenger) {
+//     flightNum = 'LH999';
+//     passenger.name = 'Mr. ' + passenger.name;
 
-    if(passenger.passport === 24739479284) {
-        alert('Checked in');
-    } else {
-        alert('Wrong passport!');
-    }
-}
+//     if(passenger.passport === 24739479284) {
+//         alert('Checked in');
+//     } else {
+//         alert('Wrong passport!');
+//     }
+// }
 
-// checkIn(flight, jonas);
-// console.log(flight);
+// // checkIn(flight, jonas);
+// // console.log(flight);
+// // console.log(jonas);
+
+// // Is the same as doing...
+// // const flightNum = flight;
+// // const passenger = jonas;
+
+// const newPassport = function(person) {
+//     person.passport = Math.trunc(Math.random() * 100000000000);
+// }
+
+// newPassport(jonas);
 // console.log(jonas);
-
-// Is the same as doing...
-// const flightNum = flight;
-// const passenger = jonas;
-
-const newPassport = function(person) {
-    person.passport = Math.trunc(Math.random() * 100000000000);
-}
-
-newPassport(jonas);
-console.log(jonas);
-checkIn(flight, jonas);
+// checkIn(flight, jonas);
