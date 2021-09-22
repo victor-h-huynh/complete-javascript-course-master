623d008c4537410300f2af9dd4822e5bb7a6865f
victor-h-huynh <victor.h.huynh@gmail.com>
2021-09-21 20:24:52 -0400

    function-accept-callback-function

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -67,3 +67,20 @@ const upperFirstWord = function(str) {
     const [first, ...others]= str.split(' ');
     return [first.toUpperCase(), ...others].join(' ')
 };
+
+// Higher-order function
+const transformer = function(str, fn) {
+    console.log(`Original string: ${str}`);
+    console.log(`Transformed string: ${fn(str)}`);
+
+    console.log(`Transformed by: ${fn.name}`)
+};
+
+transformer('Javascript is the best!', upperFirstWord);
+
+transformer('Javascript is the best!', oneWord);
+
+const high5 = function() {
+    console.log('👋')
+}
+document.body.addEventListener('click', high5);
