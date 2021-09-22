540adddf122f0c34242115eaf2afc919ca6f4a31
victor-h-huynh <victor.h.huynh@gmail.com>
2021-09-22 15:57:58 -0400

    function-accept-callback-function

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -59,31 +59,31 @@
 ///////////////////////////////////////////////////////
 // Functions Accepting Callback Functions
 
-const oneWord = function(str) {
-    return str.replace(/ /g, '').toLowerCase()
-};
+// const oneWord = function(str) {
+//     return str.replace(/ /g, '').toLowerCase()
+// };
 
-const upperFirstWord = function(str) {
-    const [first, ...others]= str.split(' ');
-    return [first.toUpperCase(), ...others].join(' ')
-};
+// const upperFirstWord = function(str) {
+//     const [first, ...others]= str.split(' ');
+//     return [first.toUpperCase(), ...others].join(' ')
+// };
 
-// Higher-order function
-const transformer = function(str, fn) {
-    console.log(`Original string: ${str}`);
-    console.log(`Transformed string: ${fn(str)}`);
+// // Higher-order function
+// const transformer = function(str, fn) {
+//     console.log(`Original string: ${str}`);
+//     console.log(`Transformed string: ${fn(str)}`);
 
-    console.log(`Transformed by: ${fn.name}`)
-};
+//     console.log(`Transformed by: ${fn.name}`)
+// };
 
-transformer('Javascript is the best!', upperFirstWord);
+// transformer('Javascript is the best!', upperFirstWord);
 
-transformer('Javascript is the best!', oneWord);
+// transformer('Javascript is the best!', oneWord);
 
-// JS uses callbacks all the time
-const high5 = function() {
-    console.log('👋');
-}
-document.body.addEventListener('click', high5);
+// // JS uses callbacks all the time
+// const high5 = function() {
+//     console.log('👋');
+// }
+// document.body.addEventListener('click', high5);
 
-['Jonas', 'Martha', 'Adam'].forEach(high5);
+// ['Jonas', 'Martha', 'Adam'].forEach(high5);
