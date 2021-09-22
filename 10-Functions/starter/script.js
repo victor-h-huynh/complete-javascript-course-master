2780a411619e85aea894f3d6760c33a7f9dac132
victor-h-huynh <victor.h.huynh@gmail.com>
2021-09-21 20:22:05 -0400

    function-accept-callback-function

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -59,3 +59,11 @@
 ///////////////////////////////////////////////////////
 // Functions Accepting Callback Functions
 
+const oneWord = function(str) {
+    return str.replace(/ /g, '').toLowerCase()
+};
+
+const upperFirstWord = function(str) {
+    const [first, ...others]= str.split(' ');
+    return [first.toUpperCase(), ...others].join(' ')
+};
