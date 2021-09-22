ed2e5ac53caadf8b30bd66a3d3b49a3d6885d181
victor-h-huynh <victor.h.huynh@gmail.com>
2021-09-22 17:36:41 -0400

    call-apply-methods

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -111,3 +111,31 @@
 ///////////////////////////////////////////////////////
 // The call and apply Methods
 
+const lufthansa = {
+    airline: 'Lufthansa',
+    iataCode: 'LH',
+    bookings: [],
+    // book: function() {}
+    book(flightNum, name) {
+        console.log(`${name} booked a seat on ${this.airline} flight ${this.iataCode}${flightNum}`);
+        this.bookings.push({flight: `${this.iataCode}${flightNum}`, name});1
+    },
+};
+
+lufthansa.book(239, 'Jonas Schmedtmann');
+lufthansa.book(635, 'John Smith');
+console.log(lufthansa);
+
+const eurowings = {
+    name: 'Eurowings',
+    iataCode: 'EW',
+    bookings: [],
+};
+
+const book = lufthansa.book;
+
+// Does NOT work
+// book(23, 'Sarah Williams');
+
+book.call(eurowings, 23, 'Sarah Williams');
+console.log(eurowings);
