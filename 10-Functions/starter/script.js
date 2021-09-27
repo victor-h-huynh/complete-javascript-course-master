dd422d3e73e34b4997327843d36dfa75dd7d84f4
victor-h-huynh <victor.h.huynh@gmail.com>
2021-09-27 16:02:00 -0400

    bind-method

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -185,3 +185,13 @@ lufthansa.buyPlane = function () {
 // lufthansa.buyPlane();
 
 document.querySelector('.buy').addEventListener('click', lufthansa.buyPlane.bind(lufthansa));
+
+// Partial Application
+const addTax = (rate, value) => value + value * rate;
+console.log(addTax(0.1, 200));
+
+const addVAT = addTax.bind(null, 0.23);
+// addVAT = value => value + value * 0.23;
+
+console.log(addVAT(100));
+console.log(addVAT(23));
