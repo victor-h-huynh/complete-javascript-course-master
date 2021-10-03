cf4eb5c66a9ad84303d54c620d194cf55decec89
victor-h-huynh <victor.h.huynh@gmail.com>
2021-10-03 17:04:41 -0400

    closures

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -319,3 +319,29 @@ GOOD LUCK 😀
 
 ///////////////////////////////////////////////////////////
 // Immediately Invoked Function Expressions (IIFE)
+
+// const runOnce = function () {
+//   console.log('This will never run again')
+// };
+
+// runOnce();
+
+// // IIFE
+// (function() {
+//   console.log('This will never run again');
+//   const isPrivate = 23;
+// })();
+
+// // console.log(isPrivate);
+
+// (() => console.log('This will ALSO never run again'))();
+
+// {
+//   const isPrivate = 23;
+//   var notPrivate = 23;
+// }
+// // console.log(isPrivate);
+// console.log(notPrivate);
+
+///////////////////////////////////////////////////////////
+// Closures
