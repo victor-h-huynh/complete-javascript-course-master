3ade3fcd43583f333b6b627b1f97736b809ad594
victor-h-huynh <victor.h.huynh@gmail.com>
2021-10-03 18:00:01 -0400

    closures-examples

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -362,3 +362,31 @@ GOOD LUCK 😀
 // booker();
 
 // console.dir(booker);
+
+///////////////////////////////////////////////////////////
+// Closures Examples
+
+let f;
+
+const g = function () {
+  const a = 23;
+  f = function () {
+    console.log(a * 2);
+  }
+}
+
+const h = function () {
+  const b = 777;
+  f = function () {
+    console.log(b * 2);
+  }
+}
+
+g();
+f();
+console.dir(f);
+
+// Re-assigning f function
+h();
+f();
+console.dir(f);
