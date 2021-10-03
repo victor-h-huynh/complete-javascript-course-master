2550b4c686c782d19f60d8127190686b1909a3fb
victor-h-huynh <victor.h.huynh@gmail.com>
2021-10-03 17:29:14 -0400

    closures

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -345,3 +345,18 @@ GOOD LUCK 😀
 
 ///////////////////////////////////////////////////////////
 // Closures
+
+const secureBooking = function() {
+  let passengerCount = 0;
+
+  return function () {
+    passengerCount++
+    console.log(`${passengerCount} passengers`)
+  }
+}
+
+const booker = secureBooking();
+
+booker();
+booker();
+booker();
