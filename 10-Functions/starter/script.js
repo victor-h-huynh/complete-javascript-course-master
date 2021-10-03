7e5223aa9d45a2771e42ea588e7f172712545e7b
victor-h-huynh <victor.h.huynh@gmail.com>
2021-10-03 18:05:48 -0400

    closures-examples

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -366,6 +366,7 @@ GOOD LUCK 😀
 ///////////////////////////////////////////////////////////
 // Closures Examples
 
+// Example 1
 let f;
 
 const g = function () {
@@ -390,3 +391,17 @@ console.dir(f);
 h();
 f();
 console.dir(f);
+
+// Example 2
+const boardPassengers = function(n, wait) {
+  const perGroup = n / 3;
+
+  setTimeout(function(){
+    console.log(`We are now boarding all ${n} passengers`);
+    console.log(`There are 3 groups, each with ${perGroup} passengers`)
+  }, wait * 1000);
+
+  console.log(`Will start boarding in ${wait} seconds`);
+};
+
+boardPassengers(180, 3)
