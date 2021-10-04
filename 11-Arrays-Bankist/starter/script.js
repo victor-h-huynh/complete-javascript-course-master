d3c7486c226bebf043085be638e92ee2ba28119b
victor-h-huynh <victor.h.huynh@gmail.com>
2021-10-04 15:26:12 -0400

    looping-array-foreach

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -71,37 +71,61 @@ const currencies = new Map([
   ['GBP', 'Pound sterling'],
 ]);
 
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
+// // Simple Array Methods
+// let arr = ['a', 'b', 'c', 'd', 'e'];
 
-let arr = ['a', 'b', 'c', 'd', 'e'];
+// // SLICE
+// console.log(arr.slice(2));
+// console.log(arr.slice(2,4));
+// console.log(arr.slice(-2));
+// console.log(arr.slice(-1));
+// console.log(arr.slice(1,-2));
+// console.log(arr.slice([...arr]));
 
-// SLICE
-console.log(arr.slice(2));
-console.log(arr.slice(2,4));
-console.log(arr.slice(-2));
-console.log(arr.slice(-1));
-console.log(arr.slice(1,-2));
-console.log(arr.slice([...arr]));
+// // SPLICE
+// // console.log(arr.splice(2));
+// arr.splice(-1);
+// console.log(arr);
+// arr.splice(1, 2);
+// console.log(arr);
 
-// SPLICE
-// console.log(arr.splice(2));
-arr.splice(-1);
-console.log(arr);
-arr.splice(1, 2);
-console.log(arr);
+// // REVERSE
+// arr = ['a', 'b', 'c', 'd', 'e'];
+// const arr2 = ['j', 'i', 'h', 'g', 'f'];
+// console.log(arr2.reverse());
+// console.log(arr2);
 
-// REVERSE
-arr = ['a', 'b', 'c', 'd', 'e'];
-const arr2 = ['j', 'i', 'h', 'g', 'f'];
-console.log(arr2.reverse());
-console.log(arr2);
+// // CONCAT
+// const letters = arr.concat(arr2);
+// console.log(letters);
+// console.log([...arr, ...arr2]);
 
-// CONCAT
-const letters = arr.concat(arr2);
-console.log(letters);
-console.log([...arr, ...arr2]);
+// // JOIN
+// console.log(letters.join(' - '));
 
-// JOIN
-console.log(letters.join(' - '));
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+// for (const movement of movements) {
+for(const [i, movement] of movements.entries()) {
+  if(movement > 0) {
+    console.log(`You deposted ${movement}`)
+  } else {
+    console.log(`You withdrew ${Math.abs(movement)}`)
+  }
+}
+
+console.log('---- FOREACH ----')
+movements.forEach(function(movement) {
+  if(movement > 0) {
+    console.log(`You deposted ${movement}`)
+  } else {
+    console.log(`You withdrew ${Math.abs(movement)}`)
+  }
+})
+// 0: function(200)
+// 1: function(450)
+// 2: function(400)
+// ...
