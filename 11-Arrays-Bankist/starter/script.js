b3c15d4c59667f870bf7633d7c3b412fa2e2b526
victor-h-huynh <victor.h.huynh@gmail.com>
2021-11-01 19:56:33 -0400

    map-method

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -84,7 +84,7 @@ displayMovements(account1.movements)
 /////////////////////////////////////////////////
 // LECTURES
 
-// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
 // // Simple Array Methods
@@ -220,3 +220,9 @@ GOOD LUCK 😀
 // checkDogs([3, 5, 2, 12, 7], [4, 1, 15, 8, 3])
 // checkDogs([9, 16, 6, 8, 3], [10, 5, 6, 1, 4])
 
+const eurToUsd = 1.1;
+const movementsUSD = movements.map(function(mov) {
+  return mov * eurToUsd;
+})
+console.log(movements);
+console.log(movementsUSD);
