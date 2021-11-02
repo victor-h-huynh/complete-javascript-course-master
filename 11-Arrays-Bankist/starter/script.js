b88c27482776202801a53fddbd492bed39b3541d
victor-h-huynh <victor.h.huynh@gmail.com>
2021-11-02 14:27:52 -0400

    filter-method

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -254,3 +254,15 @@ const deposits = movements.filter(function(mov) {
 })
 console.log(movements);
 console.log(deposits);
+
+const depositsFor = [];
+for (const mov of movements) {
+  if (mov > 0) {
+    depositsFor.push(mov);
+  }
+}
+console.log(depositsFor);
+
+const withdrawals = movements.filter(mov => mov < 0);
+
+console.log(withdrawals);
