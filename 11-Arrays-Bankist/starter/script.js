29c9147ea82731a81063deed6221f748e522c486
victor-h-huynh <victor.h.huynh@gmail.com>
2021-11-03 14:01:32 -0400

    coding-challenge-2

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -323,22 +323,34 @@ TEST DATA 2: [16, 6, 10, 5, 6, 1, 4]
 GOOD LUCK 😀
 */
 
-const testData1 = [5, 2, 4, 1, 15, 8, 3];
+// First Attempt
+// const testData1 = [5, 2, 4, 1, 15, 8, 3];
+// const testData2 = [16, 6, 10, 5, 6, 1, 4];
 
-const calcAverageHumanAge = function(ages) {
-  const humanAge = ages.map(function(age) {
-    if(age <= 2) {
-      return 2 * age;
-    } else {
-      return 16 + age * 4;
-    }
-  }).filter(function(age) {
-    return age > 18;
-  })
-  const avg = humanAge.reduce(function(age, curr) {
-    return age + curr;
-  })
-  return avg / humanAge.length;
+// const calcAverageHumanAge = function(ages) {
+//   const humanAge = ages.map(function(age) {
+//     if(age <= 2) {
+//       return 2 * age;
+//     } else {
+//       return 16 + age * 4;
+//     }
+//   }).filter(function(age) {
+//     return age > 18;
+//   })
+//   const avg = humanAge.reduce(function(age, curr) {
+//     return age + curr;
+//   })
+//   return avg / humanAge.length;
+// };
+
+// console.log(calcAverageHumanAge(testData1));
+// console.log(calcAverageHumanAge(testData2));
+
+
+// Answer
+const calcAverageHumanAge = function (ages) {
+  const humanAges = ages.map(age => (age <= 2 ? 2 * age : 16 + age * 4));
+  console.log(humanAges);
 };
 
-console.log(calcAverageHumanAge(testData1));
+calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3])
