d49d1df3a1fc6679bec69e59f505abcefb1ac69d
victor-h-huynh <victor.h.huynh@gmail.com>
2021-11-08 14:05:24 -0500

    flat-flatMap-method

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -601,28 +601,29 @@ GOOD LUCK 😀
 // console.log(movements.every(deposit));
 // console.log(movements.filter(deposit));
 
-const arr = [[1,2,3], [4,5,6], 7,8];
-console.log(arr.flat());
+// FLAT
+// const arr = [[1,2,3], [4,5,6], 7,8];
+// console.log(arr.flat());
 
-const arrDeep = [[[1,2],3], [4,[5,6]], 7,8];
-console.log(arrDeep.flat(2));
+// const arrDeep = [[[1,2],3], [4,[5,6]], 7,8];
+// console.log(arrDeep.flat(2));
 
-// const accountMovements = accounts.map(acc => acc.movements)
-// console.log(accountMovements);
-// const allMovements = accountMovements.flat();
-// console.log(allMovements);
-// const overallBalance = allMovements.reduce((acc, mov) => (acc + mov), 0)
+// // const accountMovements = accounts.map(acc => acc.movements)
+// // console.log(accountMovements);
+// // const allMovements = accountMovements.flat();
+// // console.log(allMovements);
+// // const overallBalance = allMovements.reduce((acc, mov) => (acc + mov), 0)
+// // console.log(overallBalance);
+
+// // flat
+// const overallBalance = accounts
+//   .map(acc => acc.movements)
+//   .flat()
+//   .reduce((acc, mov) => (acc + mov), 0);
 // console.log(overallBalance);
 
-// flat
-const overallBalance = accounts
-  .map(acc => acc.movements)
-  .flat()
-  .reduce((acc, mov) => (acc + mov), 0);
-console.log(overallBalance);
-
-// flatMap
-const overallBalance2 = accounts
-  .flatMap(acc => acc.movements)
-  .reduce((acc, mov) => (acc + mov), 0);
-console.log(overallBalance);
+// // flatMap
+// const overallBalance2 = accounts
+//   .flatMap(acc => acc.movements)
+//   .reduce((acc, mov) => (acc + mov), 0);
+// console.log(overallBalance);
