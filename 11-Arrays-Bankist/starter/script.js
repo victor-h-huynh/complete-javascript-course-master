512c1d74d8f1ac8b58b3a745b987d82556d4c106
victor-h-huynh <victor.h.huynh@gmail.com>
2021-11-08 14:16:30 -0500

    sort-method

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -627,3 +627,39 @@ GOOD LUCK 😀
 //   .flatMap(acc => acc.movements)
 //   .reduce((acc, mov) => (acc + mov), 0);
 // console.log(overallBalance);
+
+// Sorting Arrays
+
+// Strings
+const owners = ['Jonas', 'Zach', 'Adam', 'Martha'];
+console.log(owners.sort());
+console.log(owners);
+
+// Numbers
+console.log(movements);
+
+// return < 0, A, B (keep order)
+// return > 0, B, A (switch order)
+
+// Ascending
+// movements.sort((a, b) => {
+//   if (a > b) 
+//     return 1;
+//   if (a < b)
+//     return -1;
+// })
+
+movements.sort((a, b) => a - b)
+console.log(movements);
+
+// Descending
+// movements.sort((a, b) => {
+//   if (a > b) 
+//     return -1;
+//   if (a < b)
+//     return 1;
+// })
+
+movements.sort((a, b) => b - a)
+
+console.log(movements);
