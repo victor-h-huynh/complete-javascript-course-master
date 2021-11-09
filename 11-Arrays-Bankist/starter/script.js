b106ca258514760c8931dc9a87741cd50fd1e181
victor-h-huynh <victor.h.huynh@gmail.com>
2021-11-09 11:42:15 -0500

    array-method-practice

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -708,9 +708,21 @@ GOOD LUCK 😀
 
 console.log(accounts);
 
+// 1.
 const bankDepositSum = accounts
 .flatMap(acc => acc.movements)
 .filter(mov => mov > 0)
-.reduce((sum, cur) => (sum + cur), 0)
+.reduce((sum, cur) => (sum + cur), 0);
 
 console.log(bankDepositSum);
+
+// 2.
+// const numDeposits1000 = accounts
+// .flatMap(acc => acc.movements)
+// .filter(mov => mov >= 1000).length;
+
+const numDeposits1000 = accounts
+.flatMap(acc => acc.movements)
+.reduce((count, cur) => (cur >= 1000 ? count + 1 : count) , 0)
+
+console.log(numDeposits1000);
