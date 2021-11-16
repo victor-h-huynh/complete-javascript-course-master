ca3662f0576999e4f5256eb42e2fbee0df6cc55d
victor-h-huynh <victor.h.huynh@gmail.com>
2021-11-16 11:51:13 -0500

    remainder-operator

--- 12-Numbers-Dates-Timers-Bankist/starter/script.js
+++ 12-Numbers-Dates-Timers-Bankist/starter/script.js
@@ -330,29 +330,29 @@ btnSort.addEventListener('click', function (e) {
 // console.log(+(2.345).toFixed(2));
 
 // The Remainder Operator
+// console.log(5 % 2);
+// console.log(5 / 2); // 5 = 2 * 2 + 1
 
-console.log(5 % 2);
-console.log(5 / 2); // 5 = 2 * 2 + 1
+// console.log(8 % 3);
+// console.log(8 / 3); // 8 = 2 * 3 + 2
 
-console.log(8 % 3);
-console.log(8 / 3); // 8 = 2 * 3 + 2
+// console.log(6 % 2);
+// console.log(6 / 2);
 
-console.log(6 % 2);
-console.log(6 / 2);
+// console.log(7 % 2);
+// console.log(7 / 2);
 
-console.log(7 % 2);
-console.log(7 / 2);
+// const isEven = n => n % 2 === 0;
+// console.log(isEven(8));
+// console.log(isEven(23));
+// console.log(isEven(514));
 
-const isEven = n => n % 2 === 0;
-console.log(isEven(8));
-console.log(isEven(23));
-console.log(isEven(514));
-
-labelBalance.addEventListener('click', function () {
-  [...document.querySelectorAll('.movements__row')].forEach(function (row, i) {
-    // 0, 2, 4, 6
-    if (i % 2 === 0) row.style.backgroundColor = 'orangered';
-    // 0, 3, 6, 9
-    if (i % 3 === 0) row.style.backgroundColor = 'blue';
-  });
-});
+// labelBalance.addEventListener('click', function () {
+//   [...document.querySelectorAll('.movements__row')].forEach(function (row, i) {
+//     // 0, 2, 4, 6
+//     if (i % 2 === 0) row.style.backgroundColor = 'orangered';
+//     // 0, 3, 6, 9
+//     if (i % 3 === 0) row.style.backgroundColor = 'blue';
+//   });
+// });
+// Nth
