78938ceab77a6767790bf02aa0ba72f1c3837474
victor-h-huynh <victor.h.huynh@gmail.com>
2021-11-15 21:14:15 -0500

    math-and-rounding

--- 12-Numbers-Dates-Timers-Bankist/starter/script.js
+++ 12-Numbers-Dates-Timers-Bankist/starter/script.js
@@ -298,3 +298,12 @@ console.log(Math.max(5, 18, '23', 11, 2));
 console.log(Math.max(5, 18, '23px', 11, 2));
 
 console.log(Math.min(5, 18, 23, 11, 2));
+
+console.log(Math.PI * Number.parseFloat('10px') ** 2);
+
+console.log(Math.trunc(Math.random() * 6) + 1);
+
+const randomInt = (min, max) =>
+  Math.trunc(Math.random() * (max - min) + 1) + min;
+// 0...1 -> 0... (max - min) -> min...max
+console.log(randomInt(10, 20));
