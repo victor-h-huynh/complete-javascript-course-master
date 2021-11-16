577ba9e10aa6f76932a81c388d02815757985852
victor-h-huynh <victor.h.huynh@gmail.com>
2021-11-16 11:57:28 -0500

    working-with-BigInt

--- 12-Numbers-Dates-Timers-Bankist/starter/script.js
+++ 12-Numbers-Dates-Timers-Bankist/starter/script.js
@@ -361,17 +361,19 @@ btnSort.addEventListener('click', function (e) {
 // Numeric Separators
 
 // 287,460,000,000
-const diameter = 287_460_000_000;
-console.log(diameter);
+// const diameter = 287_460_000_000;
+// console.log(diameter);
 
-const price = 345_99;
-console.log(price);
+// const price = 345_99;
+// console.log(price);
 
-const transferFee1 = 15_00;
-const transferFee2 = 1_500;
+// const transferFee1 = 15_00;
+// const transferFee2 = 1_500;
 
-const PI = 3.14_15;
-console.log(PI);
+// const PI = 3.14_15;
+// console.log(PI);
 
-console.log(Number('230_000'));
-console.log(parseInt('230_000'));
+// console.log(Number('230_000'));
+// console.log(parseInt('230_000'));
+
+// Working with BigInt
