3dc3adffc59e7535a3621679312a690f15d1dd10
victor-h-huynh <victor.h.huynh@gmail.com>
2021-11-15 21:04:33 -0500

    math-and-rounding

--- 12-Numbers-Dates-Timers-Bankist/starter/script.js
+++ 12-Numbers-Dates-Timers-Bankist/starter/script.js
@@ -290,3 +290,11 @@ btnSort.addEventListener('click', function (e) {
 
 // Math and Rounding
 console.log(Math.sqrt(25));
+console.log(25 ** (1 / 2));
+console.log(8 ** (1 / 3));
+
+console.log(Math.max(5, 18, 23, 11, 2));
+console.log(Math.max(5, 18, '23', 11, 2));
+console.log(Math.max(5, 18, '23px', 11, 2));
+
+console.log(Math.min(5, 18, 23, 11, 2));
