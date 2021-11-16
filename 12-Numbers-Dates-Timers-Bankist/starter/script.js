8faae48c770558fa3ee32faaa4f68da8928c73ae
victor-h-huynh <victor.h.huynh@gmail.com>
2021-11-16 15:10:43 -0500

    operations-with-dates

--- 12-Numbers-Dates-Timers-Bankist/starter/script.js
+++ 12-Numbers-Dates-Timers-Bankist/starter/script.js
@@ -476,3 +476,12 @@ btnSort.addEventListener('click', function (e) {
 // console.log(future);
 
 // Operations with Dates
+
+const future = new Date(2037, 10, 19, 15, 23);
+console.log(+future);
+
+const calcDaysPassed = (date1, date2) =>
+  (date2 - date1) / (1000 * 60 * 60 * 24);
+
+const days1 = calcDaysPassed(new Date(2037, 3, 14), new Date(2037, 3, 4));
+console.log(days1);
