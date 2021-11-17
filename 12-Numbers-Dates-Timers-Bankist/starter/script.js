4becacf1276262ad09576e4bea4b5661f6f5e557
victor-h-huynh <victor.h.huynh@gmail.com>
2021-11-16 20:44:50 -0500

    setTimeout-setInterval

--- 12-Numbers-Dates-Timers-Bankist/starter/script.js
+++ 12-Numbers-Dates-Timers-Bankist/starter/script.js
@@ -275,14 +275,18 @@ btnLoan.addEventListener('click', function (e) {
   const amount = Math.floor(inputLoanAmount.value);
 
   if (amount > 0 && currentAccount.movements.some(mov => mov >= amount * 0.1)) {
-    // Add movement
-    currentAccount.movements.push(amount);
+    {
+      // Add movement
+      setTimeout(function () {
+        currentAccount.movements.push(amount);
 
-    // Add loan date
-    currentAccount.movementsDates.push(new Date().toISOString());
+        // Add loan date
+        currentAccount.movementsDates.push(new Date().toISOString());
 
-    // Update UI
-    updateUI(currentAccount);
+        // Update UI
+        updateUI(currentAccount);
+      }, 2500);
+    }
   }
   inputLoanAmount.value = '';
 });
@@ -543,6 +547,7 @@ btnSort.addEventListener('click', function (e) {
 
 // Timers: setTimeout and setInterval
 
+// setTimeout
 const ingredients = ['olives', ''];
 const pizzaTimer = setTimeout(
   (ing1, ing2) => console.log(`Here is your pizza with ${ing1} and ${ing2}`),
@@ -552,3 +557,9 @@ const pizzaTimer = setTimeout(
 console.log('Waiting...');
 
 if (ingredients.includes('spinach')) clearTimeout(pizzaTimer);
+
+// setInterval
+setInterval(function () {
+  const now = new Date();
+  console.log(now);
+}, 1000);
