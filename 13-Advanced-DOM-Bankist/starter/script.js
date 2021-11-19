db8e4bbcb2f8ded2c5f9d5249fb40e1ace4755b9
victor-h-huynh <victor.h.huynh@gmail.com>
2021-11-19 17:00:49 -0500

    select-create-delete-elements

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -35,7 +35,7 @@ console.log(document.documentElement);
 console.log(document.head);
 console.log(document.body);
 
-document.querySelector('.header');
+const header = document.querySelector('.header');
 const allSections = document.querySelectorAll('.section');
 console.log(allSections);
 
@@ -46,3 +46,15 @@ console.log(allButtons);
 console.log(document.getElementsByClassName('btn'));
 
 // Creating and inserting elements
+const message = document.createElement('div');
+message.classList.add('cookie-message');
+// message.textContent =
+//   'We use cookies for improved functionality and analytics.';
+message.innerHTML =
+  'We use cookies for improved functionality and analytics. <button class="btn btn--close-cookie">Got it!</button>';
+// header.prepend(message);
+header.append(message);
+// header.append(message.cloneNode(true));
+
+header.before(message);
+// header.after(message);
