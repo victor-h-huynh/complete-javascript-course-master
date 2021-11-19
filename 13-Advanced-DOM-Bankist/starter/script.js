01f95f769486c867eee32e89a3e26462e2c57f74
victor-h-huynh <victor.h.huynh@gmail.com>
2021-11-19 16:51:16 -0500

    select-create-delete-elements

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -31,3 +31,18 @@ document.addEventListener('keydown', function (e) {
 });
 
 // Selecting, Creating, and Deleting Elements
+console.log(document.documentElement);
+console.log(document.head);
+console.log(document.body);
+
+document.querySelector('.header');
+const allSections = document.querySelectorAll('.section');
+console.log(allSections);
+
+document.getElementById('section--1');
+const allButtons = document.getElementsByTagName('button');
+console.log(allButtons);
+
+console.log(document.getElementsByClassName('btn'));
+
+// Creating and inserting elements
