3a5770f305f6fb3e08d9669958d8f17668f79145
victor-h-huynh <victor.h.huynh@gmail.com>
2021-11-22 15:38:43 -0500

    event-propagation

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -173,13 +173,20 @@ const randomColor = () =>
 console.log(randomColor(0, 255));
 
 document.querySelector('.nav__link').addEventListener('click', function (e) {
-  console.log('LINK');
+  this.style.backgroundColor = randomColor();
+  console.log('LINK', e.target, e.currentTarget);
+  console.log(e.currentTarget === this);
+
+  // Stop propagation
+  e.stopPropagation();
 });
 
 document.querySelector('.nav__links').addEventListener('click', function (e) {
-  console.log('LINK');
+  this.style.backgroundColor = randomColor();
+  console.log('CONTAINER', e.target, e.currentTarget);
 });
 
 document.querySelector('.nav').addEventListener('click', function (e) {
-  console.log('LINK');
+  this.style.backgroundColor = randomColor();
+  console.log('NAV', e.target, e.currentTarget);
 });
