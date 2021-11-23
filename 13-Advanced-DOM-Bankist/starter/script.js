bab2bb109f9e769321f4df533d6e7be607d85e79
victor-h-huynh <victor.h.huynh@gmail.com>
2021-11-23 15:59:34 -0500

    dom-traversing

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -228,3 +228,16 @@ const h1 = document.querySelector('h1');
 // Going downwards: chil
 console.log(h1.querySelectorAll('.highlight'));
 console.log(h1.childNodes);
+console.log(h1.children);
+h1.firstElementChild.style.color = 'white';
+h1.lastElementChild.style.color = 'orangered';
+
+// Going upwards: parents
+console.log(h1.parentNode);
+console.log(h1.parentElement);
+
+h1.closest('.header').style.background = 'var(--gradient-secondary)';
+
+h1.closest('h1').style.background = 'var(--gradient-primary)';
+
+// Going sideways: siblings
