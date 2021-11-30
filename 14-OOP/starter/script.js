eb821a1db7aa08162cf7da225eee12415074e368
victor-h-huynh <victor.h.huynh@gmail.com>
2021-11-30 16:23:52 -0500

    prototypes

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -27,6 +27,18 @@ console.log(jack);
 console.log(jonas instanceof Person);
 
 // Prototypes
+console.log(Person.prototype);
+
 Person.prototype.calcAge = function () {
   console.log(2037 - this.birthYear);
 };
+
+jonas.calcAge();
+matilda.calcAge();
+
+console.log(jonas.__proto__);
+console.log(jonas.__proto__ === Person.prototype);
+
+console.log(Person.prototype.isPrototypeOf(jonas));
+console.log(Person.prototype.isPrototypeOf(matilda));
+console.log(Person.prototype.isPrototypeOf(Person));
