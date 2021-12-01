b8c56defc24c020231ec17837e5cb22af01823e5
victor-h-huynh <victor.h.huynh@gmail.com>
2021-12-01 16:30:01 -0500

    setters-and-getters

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -125,26 +125,34 @@ GOOD LUCK 😀
 // }
 
 // class declaration
-class PersonCl {
-  constructor(firstName, birthYear) {
-    this.firstName = firstName;
-    this.birthYear = birthYear;
-  }
+// class PersonCl {
+//   constructor(firstName, birthYear) {
+//     this.firstName = firstName;
+//     this.birthYear = birthYear;
+//   }
 
-  // Methods will be added to .prototype property
-  calcAge() {
-    console.log(2037 - this.birthYear);
-  }
-}
+//   // Methods will be added to .prototype property
+//   calcAge() {
+//     console.log(2037 - this.birthYear);
+//   }
 
-const jessica = new PersonCl('Jessica', 1996);
-console.log(jessica);
-jessica.calcAge();
+//   greet() {
+//     console.log(`Hey ${this.firstName}`);
+//   }
+// }
 
-console.log(jessica.__proto__ === PersonCl.prototype);
+// const jessica = new PersonCl('Jessica', 1996);
+// console.log(jessica);
+// jessica.calcAge();
 
-PersonCl.prototype.greet = function () {
-  console.log(`Hey ${this.firstName}`);
-};
+// console.log(jessica.__proto__ === PersonCl.prototype);
 
-jessica.greet();
+// // PersonCl.prototype.greet = function () {
+// //   console.log(`Hey ${this.firstName}`);
+// // };
+
+// jessica.greet();
+
+// 1. Classes are NOT hoisted
+// 2. Class are first-class citizens
+// 3. Classes are executed in strict mode
