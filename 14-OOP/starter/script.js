b264b9eb4573e0bba12088d14d79d4dbe5cdde80
victor-h-huynh <victor.h.huynh@gmail.com>
2021-12-01 16:24:00 -0500

    es6-classes

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -115,3 +115,36 @@ GOOD LUCK 😀
 
 // console.log(bmw);
 // console.log(mercedes);
+
+/////////////////////////////////////
+// ES6 Classes
+
+// class expression
+// const PersonCl = class {
+
+// }
+
+// class declaration
+class PersonCl {
+  constructor(firstName, birthYear) {
+    this.firstName = firstName;
+    this.birthYear = birthYear;
+  }
+
+  // Methods will be added to .prototype property
+  calcAge() {
+    console.log(2037 - this.birthYear);
+  }
+}
+
+const jessica = new PersonCl('Jessica', 1996);
+console.log(jessica);
+jessica.calcAge();
+
+console.log(jessica.__proto__ === PersonCl.prototype);
+
+PersonCl.prototype.greet = function () {
+  console.log(`Hey ${this.firstName}`);
+};
+
+jessica.greet();
