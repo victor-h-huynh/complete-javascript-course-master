2a7d04b612d74f7483ef2e459c3fc95d725781ee
victor-h-huynh <victor.h.huynh@gmail.com>
2021-12-03 14:12:40 -0500

    inheritance-between-classes-constructor-function

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -26,6 +26,12 @@
 
 // console.log(jonas instanceof Person);
 
+// Person.hey = function () {
+//   console.log('Hey there!');
+//   console.log(this);
+// };
+// Person.hey();
+
 // // Prototypes
 // console.log(Person.prototype);
 
@@ -126,11 +132,12 @@ GOOD LUCK 😀
 
 // class declaration
 // class PersonCl {
-//   constructor(firstName, birthYear) {
-//     this.firstName = firstName;
+//   constructor(fullName, birthYear) {
+//     this.fullName = fullName;
 //     this.birthYear = birthYear;
 //   }
 
+//   // Instance methods
 //   // Methods will be added to .prototype property
 //   calcAge() {
 //     console.log(2037 - this.birthYear);
@@ -139,11 +146,33 @@ GOOD LUCK 😀
 //   greet() {
 //     console.log(`Hey ${this.firstName}`);
 //   }
+
+//   get age() {
+//     return 2037 - this.birthYear;
+//   }
+
+//   // Set a property that already exists
+//   set fullName(name) {
+//     console.log(name);
+//     if (name.includes(' ')) this._fullName = name;
+//     else alert(`${name} is not a full name!`);
+//   }
+
+//   get fullName() {
+//     return this._fullName;
+//   }
+
+//   // Static
+//   static hey() {
+//     console.log('Hey there!');
+//     console.log(this);
+//   }
 // }
 
-// const jessica = new PersonCl('Jessica', 1996);
+// const jessica = new PersonCl('Jessica Davis', 1996);
 // console.log(jessica);
 // jessica.calcAge();
+// console.log(jessica.age);
 
 // console.log(jessica.__proto__ === PersonCl.prototype);
 
@@ -156,3 +185,100 @@ GOOD LUCK 😀
 // 1. Classes are NOT hoisted
 // 2. Class are first-class citizens
 // 3. Classes are executed in strict mode
+
+/////////////////////////////////////
+// Setters and Getters
+
+// const walter = new PersonCl('Walter White', 1965);
+// PersonCl.hey();
+
+// const account = {
+//   owner: 'jonas',
+//   movements: [200, 530, 120, 300],
+
+//   get latest() {
+//     return this.movements.slice(-1).pop();
+//   },
+
+//   set latest(mov) {
+//     this.movements.push(mov);
+//   },
+// };
+
+// console.log(account.latest);
+
+// account.latest = 50;
+// console.log(account.movements);
+
+// const PersonProto = {
+//   calcAge() {
+//     console.log(2037 - this.birthYear);
+//   },
+
+//   init(firstName, birthYear) {
+//     this.firstName = firstName;
+//     this.birthYear = birthYear;
+//   },
+// };
+
+// const steven = Object.create(PersonProto);
+// console.log(steven);
+// steven.name = 'Steven';
+// steven.birthYear = 2002;
+// steven.calcAge();
+
+// console.log(steven.__proto__ === PersonProto);
+
+// const sarah = Object.create(PersonProto);
+
+// sarah.init('Sarah', 1979);
+// sarah.calcAge();
+
+///////////////////////////////////////
+// Coding Challenge #2
+
+/* 
+1. Re-create challenge 1, but this time using an ES6 class;
+2. Add a getter called 'speedUS' which returns the current speed in mi/h (divide by 1.6);
+3. Add a setter called 'speedUS' which sets the current speed in mi/h (but converts it to km/h before storing the value, by multiplying the input by 1.6);
+4. Create a new car and experiment with the accelerate and brake methods, and with the getter and setter.
+
+DATA CAR 1: 'Ford' going at 120 km/h
+
+GOOD LUCK 😀
+*/
+
+// class CarCl {
+//   constructor(make, speed) {
+//     this.make = make;
+//     this.speed = speed;
+//   }
+
+//   accelerate() {
+//     this.speed += 10;
+//     console.log(`${this.make} is going at ${this.speed} km/h`);
+//   }
+
+//   brake() {
+//     this.speed -= 5;
+//     console.log(`${this.make} is going at ${this.speed} km/h`);
+//   }
+
+//   get speedUS() {
+//     return this.speed / 1.6;
+//   }
+//   set speedUS(speed) {
+//     this.speed = speed * 1.6;
+//   }
+// }
+
+// const ford = new CarCl('Ford', 120);
+
+// ford.accelerate();
+// ford.accelerate();
+// ford.brake();
+// ford.speedUS = 50;
+// console.log(ford);
+
+//////////////////////////////////////////////////////////
+// Inheritance Between "Classes": Constructor Functions
