5afcd6a019a420b040a88d30e6dd2807cbd3188f
victor-h-huynh <victor.h.huynh@gmail.com>
2022-01-06 14:41:16 -0500

    ajax-call-XMLHttpRequest

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -5,3 +5,5 @@ const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
+
+const request = new XMLHttpRequest();
