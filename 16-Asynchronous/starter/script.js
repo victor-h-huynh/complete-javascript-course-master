9fb29b9f2e166f3d0764a6f68ed07dd53c8f38d9
victor-h-huynh <victor.h.huynh@gmail.com>
2022-01-06 16:16:07 -0500

    ajax-call-XMLHttpRequest

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -7,15 +7,21 @@ const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
 
-const request = new XMLHttpRequest();
-request.open('GET', 'https://restcountries.com/v3.1/name/vietnam');
-request.send();
+const getCountryData = function (country) {
+  const request = new XMLHttpRequest();
+  request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
+  request.send();
 
-request.addEventListener('load', function () {
-  const [data] = JSON.parse(this.responseText);
-  console.log(data);
+  request.addEventListener('load', function () {
+    const [data] = JSON.parse(this.responseText);
+    console.log(data);
 
-  const html = `
+    const languages = Object.values(data.languages);
+    const currencies = Object.values(data.currencies);
+    console.log('languages', languages);
+    console.log('currencies', currencies);
+
+    const html = `
 <article class="country">
     <img class="country__img" src="${data.flags.png}" />
     <div class="country__data">
@@ -24,11 +30,16 @@ request.addEventListener('load', function () {
     <p class="country__row"><span>👫</span>${(
       +data.population / 1000000
     ).toFixed(1)} people</p>
-    <p class="country__row"><span>🗣️</span>${data.languages.vie}</p>
-    <p class="country__row"><span>💰</span>${data.currencies.VND.name}</p>
+    <p class="country__row"><span>🗣️</span>${languages[0]}</p>
+    <p class="country__row"><span>💰</span>${currencies[0].name}</p>
     </div>
 </article>
 `;
-  countriesContainer.insertAdjacentHTML('beforeend', html);
-  countriesContainer.style.opacity = 1;
-});
+    countriesContainer.insertAdjacentHTML('beforeend', html);
+    countriesContainer.style.opacity = 1;
+  });
+};
+
+getCountryData('vietnam');
+getCountryData('canada');
+getCountryData('usa');
