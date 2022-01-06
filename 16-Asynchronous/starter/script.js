90dceef62168b912798dadb0670aa559ff79c1e8
victor-h-huynh <victor.h.huynh@gmail.com>
2022-01-06 15:40:48 -0500

    ajax-call-XMLHttpRequest

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -1,5 +1,6 @@
 'use strict';
 // https://restcountries.com/v2/
+// https://restcountries.com/v3.1/name/{name}
 
 const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
@@ -7,3 +8,27 @@ const countriesContainer = document.querySelector('.countries');
 ///////////////////////////////////////
 
 const request = new XMLHttpRequest();
+request.open('GET', 'https://restcountries.com/v3.1/name/vietnam');
+request.send();
+
+request.addEventListener('load', function () {
+  const [data] = JSON.parse(this.responseText);
+  console.log(data);
+
+  const html = `
+<article class="country">
+    <img class="country__img" src="${data.flags.png}" />
+    <div class="country__data">
+    <h3 class="country__name">${data.name.common}</h3>
+    <h4 class="country__region">${data.region}</h4>
+    <p class="country__row"><span>👫</span>${(
+      +data.population / 1000000
+    ).toFixed(1)} people</p>
+    <p class="country__row"><span>🗣️</span>${data.languages.vie}</p>
+    <p class="country__row"><span>💰</span>${data.currencies.VND.name}</p>
+    </div>
+</article>
+`;
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = 1;
+});
