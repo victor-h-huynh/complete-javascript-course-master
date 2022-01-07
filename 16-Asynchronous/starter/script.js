5ff49e570f95bce0ae5e0e2578917669528344b6
victor-h-huynh <victor.h.huynh@gmail.com>
2022-01-06 19:09:07 -0500

    chaining-promises

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -8,28 +8,28 @@ const countriesContainer = document.querySelector('.countries');
 ///////////////////////////////////////
 // Our First AJAX Call: XMLHttpRequest
 
-// const renderCountry = function (data, className = '') {
-//   const languages = Object.values(data.languages);
-//   const currencies = Object.values(data.currencies);
+const renderCountry = function (data, className = '') {
+  //   const languages = Object.values(data.languages);
+  //   const currencies = Object.values(data.currencies);
+  console.log(data.name);
 
-//   const html = `
-// <article class="country ${className}">
-//     <img class="country__img" src="${data.flags.png}" />
-//     <div class="country__data">
-//     <h3 class="country__name">${data.name.common}</h3>
-//     <h4 class="country__region">${data.region}</h4>
-//     <p class="country__row"><span>👫</span>${(
-//       +data.population / 1000000
-//     ).toFixed(1)} people</p>
-//     <p class="country__row"><span>🗣️</span>${languages[0]}</p>
-//     <p class="country__row"><span>💰</span>${currencies[0].name}</p>
-//     </div>
-// </article>
-// `;
-//   countriesContainer.insertAdjacentHTML('beforeend', html);
-//   countriesContainer.style.opacity = 1;
-// };
-
+  const html = `
+<article class="country ${className}">
+    <img class="country__img" src="${data.flags.png}" />
+    <div class="country__data">
+    <h3 class="country__name">${data.name}</h3>
+    <h4 class="country__region">${data.region}</h4>
+    <p class="country__row"><span>👫</span>${(
+      +data.population / 1000000
+    ).toFixed(1)} people</p>
+    <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+    <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
+    </div>
+</article>
+`;
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = 1;
+};
 // const getCountryDataAndNeighbour = function (country) {
 //   // AJAX call country 1
 //   const request = new XMLHttpRequest();
@@ -77,11 +77,33 @@ const countriesContainer = document.querySelector('.countries');
 //   }, 1000);
 // }, 1000);
 
+// const getCountryData = function (country) {
+//   fetch(`https://restcountries.com/v3.1/name/${country}`)
+//     .then(function (response) {
+//       console.log(response);
+//       return response.json();
+//     })
+//     .then(function (data) {
+//       console.log(data);
+//       renderCountry(data[0]);
+//     });
+// };
+
 const getCountryData = function (country) {
-  fetch(`https://restcountries.com/v3.1/name/${country}`).then(function (
-    response
-  ) {
-    console.log(response);
-  });
+  // Country 1
+  fetch(`https://restcountries.com/v2/name/${country}`)
+    .then(response => response.json())
+    .then(data => {
+      renderCountry(data[0]);
+      const neighbour = data[0].borders[0];
+
+      if (!neighbour) return;
+
+      // Country 2
+      return fetch(`https://restcountries.com/v2/alpha/${neighbour}`);
+    })
+    .then(response => response.json())
+    .then(data => renderCountry(data, 'neighbour'));
 };
+
 getCountryData('vietnam');
