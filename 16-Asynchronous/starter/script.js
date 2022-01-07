82ddf21399d8f73e56b2eddcb8586f3308d985cf
victor-h-huynh <victor.h.huynh@gmail.com>
2022-01-07 15:24:02 -0500

    throwing-errors-manually

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -10,19 +10,22 @@ const countriesContainer = document.querySelector('.countries');
 
 const renderCountry = function (data, className = '') {
   const html = `
-<article class="country ${className}">
-    <img class="country__img" src="${data.flags.png}" />
-    <div class="country__data">
-    <h3 class="country__name">${data.name}</h3>
-    <h4 class="country__region">${data.region}</h4>
-    <p class="country__row"><span>👫</span>${(
-      +data.population / 1000000
-    ).toFixed(1)} people</p>
-    <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
-    <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
-    </div>
-</article>
-`;
+  <article class="country ${className}">
+      <img class="country__img" src="${data.flags.png}" />
+      <div class="country__data">
+          <h3 class="country__name">${data.name.common}</h3>
+          <h4 class="country__region">${data.region}</h4>
+          <p class="country__row"><span>👫</span>${Number(
+            data.population / 1000000
+          ).toFixed()}M people</p>
+          <p class="country__row"><span>🗣️</span>${
+            Object.values(data.languages)[0]
+          }</p>
+          <p class="country__row"><span>💰</span>${
+            Object.values(data.currencies)[0].name
+          }</p>
+      </div>
+  </article>`;
   countriesContainer.insertAdjacentHTML('beforeend', html);
   //   countriesContainer.style.opacity = 1;
 };
@@ -92,16 +95,22 @@ const renderError = function (msg) {
 
 const getCountryData = function (country) {
   // Country 1
-  fetch(`https://restcountries.com/v2/name/${country}`)
-    .then(response => response.json())
+  fetch(`https://restcountries.com/v3.1/name/${country}`)
+    .then(response => {
+      console.log(response);
+
+      if (!response.ok) throw new Error(`Country not found ${response.status}`);
+
+      return response.json();
+    })
     .then(data => {
       renderCountry(data[0]);
-      const neighbour = data[0].borders[0];
+      const neighbour = data[0].borders;
 
       if (!neighbour) return;
 
       // Country 2
-      return fetch(`https://restcountries.com/v2/alpha/${neighbour}`);
+      return fetch(`https://restcountries.com/v3.1/alpha/${neighbour[0]}`);
     })
     .then(response => response.json())
     .then(data => renderCountry(data, 'neighbour'))
@@ -118,4 +127,4 @@ btn.addEventListener('click', function () {
   getCountryData('vietnam');
 });
 
-getCountryData('asdgs');
+getCountryData('fdokgfdiogd');
