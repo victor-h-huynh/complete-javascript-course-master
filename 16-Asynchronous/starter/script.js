f6711571fbb23a9dd91b1fb5673b386e685c97be
victor-h-huynh <victor.h.huynh@gmail.com>
2022-01-11 13:59:47 -0500

    coding-challenge-2

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -311,3 +311,49 @@ TEST DATA: Images in the img folder. Test the error handler by passing a wrong i
 
 GOOD LUCK 😀
 */
+
+const imgContainer = document.querySelector('.images');
+
+const wait = function (seconds) {
+  return new Promise(function (resolve) {
+    setTimeout(resolve, seconds * 1000);
+  });
+};
+
+const createImage = function (imgPath) {
+  return new Promise(function (resolve, reject) {
+    const img = document.createElement('img');
+    img.src = imgPath;
+
+    img.addEventListener('load', function () {
+      imgContainer.append(img);
+      resolve(img);
+    });
+    img.addEventListener('error', function () {
+      reject(new Error('Image not found'));
+    });
+  });
+};
+
+let currentImg;
+
+createImage('img/img-1.jpg')
+  .then(img => {
+    currentImg = img;
+    return wait(2);
+  })
+  .then(img => {
+    currentImg.style.display = 'none';
+    createImage('img/img-2.jpg');
+    currentImg = img;
+    return wait(2);
+  })
+  .then(() => {
+    currentImg.style.display = 'none';
+    return wait(2);
+  })
+  .catch(err => console.error(`ERROR IN THE CATCH ${err}`));
+
+// let img = document.createElement('img');
+// img.src = 'img/img-1.jpg';
+// document.body.appendChild(img);
