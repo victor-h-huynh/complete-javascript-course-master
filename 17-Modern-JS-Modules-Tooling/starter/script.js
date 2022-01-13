b3dc4ea975051180c71f283064bc6bc69fd9979e
victor-h-huynh <victor.h.huynh@gmail.com>
2022-01-13 13:41:12 -0500

    exporting-importing-es6-modules

--- 17-Modern-JS-Modules-Tooling/starter/script.js
+++ 17-Modern-JS-Modules-Tooling/starter/script.js
@@ -1,3 +1,10 @@
 'use strict';
 // Importing Modules
+// import { addToCart, totalPrice as price, tq } from './shoppingCart.js';
+// addToCart('bread', 5);
+// console.log(price, tq);
+
+import * as ShoppingCart from './shoppingCart';
+
 console.log('Importing module');
+// console.log(shippingCost)
