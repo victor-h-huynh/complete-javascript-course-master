5060635bf9bb978a333a3d030b4d3f77de48e2d5
victor-h-huynh <victor.h.huynh@gmail.com>
2022-01-13 16:15:35 -0500

    top-level-await

--- 17-Modern-JS-Modules-Tooling/starter/shoppingCart.js
+++ 17-Modern-JS-Modules-Tooling/starter/shoppingCart.js
@@ -2,9 +2,9 @@
 console.log('Exporting module');
 
 // Blocking code
-console.log('Start fetching users');
-await fetch('https://jsonplaceholder.typicode.com/users');
-console.log('Finish fetching users');
+// console.log('Start fetching users');
+// await fetch('https://jsonplaceholder.typicode.com/users');
+// console.log('Finish fetching users');
 
 const shippingCost = 10;
 export const cart = [];
