7c4bc5b12c023af938d28c6b5fc96c7d09a52243
victor-h-huynh <victor.h.huynh@gmail.com>
2022-01-13 16:14:53 -0500

    top-level-await

--- 17-Modern-JS-Modules-Tooling/starter/script.js
+++ 17-Modern-JS-Modules-Tooling/starter/script.js
@@ -29,6 +29,15 @@ console.log(cart);
 const getLastPost = async function () {
   const res = await fetch('https://jsonplaceholder.typicode.com/posts');
   const data = await res.json();
-  console.log(data);
+
+  return { title: data.at(-1).title, text: data.at(-1).body };
 };
-getLastPost();
+
+const lastPost = getLastPost();
+console.log(lastPost);
+
+// Not very clean
+// lastPost.then(last => console.log(last));
+
+const lastPost2 = await getLastPost();
+console.log(lastPost2);
