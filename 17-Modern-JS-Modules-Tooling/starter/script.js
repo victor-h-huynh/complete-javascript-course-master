650a08cad5674b3db524c06422723c9a853edacd
victor-h-huynh <victor.h.huynh@gmail.com>
2022-01-14 14:27:38 -0500

    configuring-babel-and-polyfilling

--- 17-Modern-JS-Modules-Tooling/starter/script.js
+++ 17-Modern-JS-Modules-Tooling/starter/script.js
@@ -123,3 +123,13 @@ class Person {
 const jonas = new Person('Jonas');
 
 console.log('Jonas' ?? null);
+
+console.log(cart.find(el => el.quantity >= 2));
+Promise.resolve('TEST').then(x => console.log(x));
+
+import 'core-js/stable';
+// import 'core-js/stable/array/find';
+// import 'core-js/stable/promise';
+
+// Polyfiling async functions
+import 'regenerator-runtime/runtime';
