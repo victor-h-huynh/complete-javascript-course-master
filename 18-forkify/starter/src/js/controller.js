bd77df3fba2bfeeeba5236d0cb1d5710c6a3e52d
victor-h-huynh <victor.h.huynh@gmail.com>
2022-01-20 04:25:17 -0500

    implementing-search-results

--- 18-forkify/starter/src/js/controller.js
+++ 18-forkify/starter/src/js/controller.js
@@ -39,7 +39,8 @@ const controlSearchResults = async function () {
     await model.loadSearchResults(query);
 
     // 3) Render results
-    resultsView.render(model.state.search.results);
+    // resultsView.render(model.state.search.results);
+    resultsView.render(model.getSearchResultsPage(1));
   } catch (err) {
     console.log(err);
   }
